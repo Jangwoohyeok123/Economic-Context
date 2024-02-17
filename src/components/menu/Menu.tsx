a10cdf07e199abd6404f5ceb152eadf99da9e5ee
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-02-17 16:58:23 +0900

    feat: Menu 컴포넌트 탭 기능 추가
    Categorys, MyContext 로 탭을 분리했다. 탭의 정보는 dashboard 헤더와 관련되며 MyContext 는 이후 자식 Tab 배열이 더필요하다.

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,16 +1,22 @@
-import clsx from "clsx";
-import styles from "./Menu.module.scss";
+import clsx from 'clsx';
+import styles from './Menu.module.scss';
+import Link from 'next/link';
 
-export default function Menu() {
-  return (
-    <div className={clsx(styles.Menu)}>
-      <nav>
-        <span>장바구니</span>
-        <span>Context</span>
-        <span>Umnmm</span>
-        <span>Hello world</span>
-        <span>DDDD</span>
-      </nav>
-    </div>
-  );
+export default function Menu({ Tabs, SelectedIdx, setSelectedIdx }) {
+	return (
+		<aside className={clsx(styles.Menu)}>
+			<nav>
+				<Link href='/' className={clsx(styles.logo)}>
+					EconomicContext
+				</Link>
+				{Tabs.map((name, idx) => {
+					return (
+						<span key={idx} onClick={() => setSelectedIdx(idx)} className={SelectedIdx === idx ? styles.on : ''}>
+							{name}
+						</span>
+					);
+				})}
+			</nav>
+		</aside>
+	);
 }
