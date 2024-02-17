ac5fb01247240de2e8a8025b5863267762215053
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-02-17 17:00:06 +0900

    feat: dashboard 페이지 header 컴포넌트 제외 및 jsx 로 dashboard 컴포넌트 청사진 잡기
    menu 에서 state 를 전달받아 header 에 연동시키는 작업을 위해 컴포넌트 분리전 jsx 로 작업함

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,19 +1,27 @@
-import clsx from "clsx";
-import styles from "./Dashboard.module.scss";
-import { Roboto } from "next/font/google";
-import Menu from "@/components/menu/Menu";
+import clsx from 'clsx';
+import styles from './Dashboard.module.scss';
+import { Roboto } from 'next/font/google';
+import Menu from '@/components/menu/Menu';
+import { useState } from 'react';
 
 const roboto = Roboto({
-  subsets: ["latin"],
-  weight: ["300", "400", "500"],
-  variable: "--baseFont",
+	subsets: ['latin'],
+	weight: ['300', '400', '500'],
+	variable: '--baseFont'
 });
 
 export default function Dashboard() {
-  return (
-    <main className={clsx(styles.Dashboard, roboto.variable)}>
-      <Menu />
-      <div className={clsx(styles.wrap)}>asdasdsazxchgzxvcghjvzxc</div>
-    </main>
-  );
+	const [Tabs] = useState(['Indicators', 'MyContext']);
+	const [SelectedIdx, setSelectedIdx] = useState(0);
+
+	return (
+		<div className={clsx(styles.Dashboard, roboto.variable)}>
+			<Menu Tabs={Tabs} SelectedIdx={SelectedIdx} setSelectedIdx={setSelectedIdx} />
+			<header className={clsx(styles.dashHeader)}>
+				<h2>{Tabs[SelectedIdx]}</h2>
+				<span>Userid</span>
+			</header>
+			<div className={clsx(styles.wrap)}></div>
+		</div>
+	);
 }
