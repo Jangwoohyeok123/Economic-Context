60246e0b8b8a3ca7ab34091255b9747903b4181d
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-02-26 21:59:43 +0900

    refactor: 컴포넌트 분리 및 전역 state 연결
    header, Indicator, Menu 및 이후의 context 컴포넌트 분리를 통해 pages 코드의 가독성을 증가시킴

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,27 +1,97 @@
-import clsx from 'clsx';
+import { roboto, poppins } from '../index';
 import styles from './Dashboard.module.scss';
-import { Roboto } from 'next/font/google';
+import clsx from 'clsx';
+import { useContext, useEffect, useState } from 'react';
 import Menu from '@/components/menu/Menu';
-import { useState } from 'react';
+import Dashheader from '@/components/dashheader/DashHeader';
+import Indicators from '@/components/indicators/Indicators';
+import { CheckedCardSetContext } from '@/contexts/checkedCardSetContext';
 
-const roboto = Roboto({
-	subsets: ['latin'],
-	weight: ['300', '400', '500'],
-	variable: '--baseFont'
-});
+const 사용자가_선택한_카테고리_카드데이터_Consume = [
+	{
+		title: '10년물 국채'
+		// checked: true
+	},
+	{
+		title: '20년물 국채'
+	},
+	{
+		title: '20년물 국채'
+	}
+];
+
+const 사용자가_선택한_카테고리_카드데이터_Exchange = [
+	{
+		title: '10년물 국채'
+		// checked: true
+	},
+	{
+		title: '20년물 국채'
+	},
+	{
+		title: '20년물 국채'
+	}
+];
+
+const 사용자가_선택한_카테고리_카드데이터_Interest = [
+	{
+		title: '10년물 국채'
+		// checked: true
+	},
+	{
+		title: '20년물 국채'
+	},
+	{
+		title: '20년물 국채'
+	}
+];
 
 export default function Dashboard() {
 	const [Tabs] = useState(['Indicators', 'MyContext']);
-	const [SelectedIdx, setSelectedIdx] = useState(0);
+	const [TabsIndex, setSelectedIdx] = useState(0);
+	const { checkedCardSet, setCheckedCardSet } = useContext(CheckedCardSetContext);
+
+	useEffect(() => {
+		console.log(checkedCardSet);
+	}, [checkedCardSet]);
+
+	// Categorys 에 필요한 데이터: 사용자가 선택했던 카드데이터들
+	const [Categorys] = useState([
+		{
+			title: 'Interest',
+			clientCheckedData: ['10년물 국채', '20년물 국채', '30년물 국채']
+		},
+		{
+			title: 'Exchange',
+			clientCheckedData: ['10년물 환율', '20년물 환율', '30년물 환율']
+		},
+		{
+			title: 'Consume',
+			clientCheckedData: ['10년물 소비', '20년물 소비', '30년물 소비']
+		},
+		{
+			title: 'Production',
+			clientCheckedData: ['10년물 생산', '20년물 생산', '30년물 생산']
+		}
+	]);
+	const [CategoryIndex, setCategoryIndex] = useState(0);
 
 	return (
-		<div className={clsx(styles.Dashboard, roboto.variable)}>
-			<Menu Tabs={Tabs} SelectedIdx={SelectedIdx} setSelectedIdx={setSelectedIdx} />
-			<header className={clsx(styles.dashHeader)}>
-				<h2>{Tabs[SelectedIdx]}</h2>
-				<span>Userid</span>
-			</header>
-			<div className={clsx(styles.wrap)}></div>
+		<div className={clsx(styles.Dashboard, roboto.variable, poppins.variable)}>
+			<Menu Tabs={Tabs} SelectedIdx={TabsIndex} setSelectedIdx={setSelectedIdx} />
+
+			{/* section 에 padding 을 주면 header 가 문제생겨서 padding 을 각 요소에 나눠줌 */}
+			<section>
+				<Dashheader Tabs={Tabs} TabsIndex={TabsIndex} />
+
+				{Tabs[TabsIndex] === 'Indicators' ? (
+					<Indicators Categorys={Categorys} CategoryIndex={CategoryIndex} setCategoryIndex={setCategoryIndex} />
+				) : (
+					<div>MyContext</div>
+				)}
+			</section>
 		</div>
 	);
 }
+// 탭을 이동할 시 clinetCategoryCheckedArr[idx] 의 값을 할당한다.
+//
