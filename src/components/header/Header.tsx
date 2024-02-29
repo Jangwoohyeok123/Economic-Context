8db01b8393f5fe11dd0208fd952dc456c321fc0f
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-02-29 21:45:59 +0900

    feat: header 에 login 여부 반영
    - login 시 myContext 이동 및 logout
    - logout 시 myContext 는 login 페이지 유도
    - login 은 login page 로 이동

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,6 +4,8 @@ import { Poppins } from 'next/font/google';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import { useContext } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout } from '@/actions/actions';
 
 interface HeaderProps {
 	children: React.ReactNode;
@@ -16,17 +18,41 @@ const poppins = Poppins({
 });
 
 export default function Header() {
+	const isLogin = useSelector(state => state.user.isLogin);
 	const router = useRouter();
+	const dispatch = useDispatch();
+
+	const goToLoginPage = () => {
+		console.log(isLogin);
+		if (!isLogin) {
+			alert('custom alert 으로 로그인유도');
+			window.location.href = 'http://localhost:3000/login';
+		}
+	};
+
+	const userLogout = () => {
+		console.log(isLogin);
+		alert('modal logout 알림');
+		dispatch(logout());
+		// window.location.href = 'http://localhost:3000';
+	};
 
 	// 나중에 조건부 렌더링 컴포넌트 분리
 	return (
 		<header className={clsx(styles.Header, poppins.variable)}>
 			<nav className={clsx(styles.mainNav)}>
 				<Link href='/'>EconomicContext</Link>
-				<div className={clsx(styles.users)}>
-					<Link href='/dashboard'>MyContext</Link>
-					<Link href='/login'>Login</Link>
-				</div>
+				{isLogin ? (
+					<div className={clsx(styles.users)}>
+						<Link href='/dashboard'>MyContext</Link>
+						<span onClick={userLogout}>Logout</span>
+					</div>
+				) : (
+					<div className={clsx(styles.users)}>
+						<span onClick={goToLoginPage}>MyContext</span>
+						<Link href='/login'>Login</Link>
+					</div>
+				)}
 			</nav>
 		</header>
 	);
