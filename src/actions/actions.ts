536f178b8b5e38f04091f2f2b318ba63894c51c8
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-03-01 19:22:02 +0900

    feat: redux action 및 reducer 추가
    - user state 추가
    - savedCardSet 추가

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,3 +1,4 @@
+// user
 export const login = () => {
 	return { type: 'login' };
 };
@@ -5,3 +6,16 @@ export const login = () => {
 export const logout = () => {
 	return { type: 'logout' };
 };
+
+// savedCardSet
+// 카드를 추가하는 액션 생성자
+export const addCard = (category: string, seriesId: string, title: string) => ({
+	type: 'add',
+	payload: { category, card: { seriesId, title } }
+});
+
+// 카드를 삭제하는 액션 생성자
+export const removeCard = (category: string, seriesId: string) => ({
+	type: 'remove',
+	payload: { category, seriesId }
+});
--- src/reducers/reducer.ts
+++ src/reducers/reducer.ts
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 
 const userInitialState = {
-	isLogin: false,
+	isLogin: true,
 	userId: 'JWH'
 };
 
@@ -12,8 +12,45 @@ const userReducer = (state = userInitialState, action) => {
 	return state;
 };
 
+const savedCardSetState = {
+	interest: [],
+	exchange: [],
+	consume: [],
+	production: []
+};
+
+interface savedCardSetAction {
+	type: string;
+	payload: {
+		category: string;
+		card: {
+			seriesId: string;
+			title: string;
+		};
+	};
+}
+
+const savedCardSetReducer = (state = savedCardSetState, action) => {
+	if (action.type === 'add') {
+		const { category, card } = action.payload;
+		return {
+			...state,
+			[category]: [...state[category], card]
+		};
+	} else if (action.type === 'remove') {
+		const { category, seriesId } = action.payload;
+		return {
+			...state,
+			[category]: state[category].filter(card => card.seriesId !== seriesId)
+		};
+	}
+
+	return state;
+};
+
 const combinedReducers = combineReducers({
-	user: userReducer
+	user: userReducer,
+	savedCardSet: savedCardSetReducer
 });
 
 export default combinedReducers;
