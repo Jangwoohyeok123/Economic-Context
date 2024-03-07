a39a9fb85a8c3c52e38f8b11125d0d155522569d
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-03-07 13:07:40 +0900

    feat: pages/api ts 로 변경 및 category 추가

---
+++ src/pages/api/category.ts
@@ -0,0 +1,40 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+type Data = {
+	name: string;
+};
+
+interface Observation {
+	date: string;
+	realtime_end: string;
+	realtime_start: string;
+	value: string;
+}
+
+type ApiResponse = {
+	observations?: Observation[];
+	message?: string;
+};
+
+// 일반적으로 return 문을 사용하지 않음
+// req: 클라이언트 요청 객체
+// res: 서버 응답객체
+
+export default async function getCategory(req: NextApiRequest, res: NextApiResponse<ApiResponse>) {
+	const baseUrl = process.env.NEXT_PUBLIC_FRED_BASEURL;
+	const apiKey = process.env.NEXT_PUBLIC_FREDKEY;
+
+	try {
+		const { categoryId } = req.query;
+
+		const response = await fetch(
+			`${baseUrl}category/series?category_id=${categoryId}&api_key=${apiKey}&file_type=json`
+		);
+		const json = await response.json();
+
+		res.status(200).json({ category: json });
+	} catch (err) {
+		res.status(500).json({ message: 'fetching 실패' });
+	}
+}
