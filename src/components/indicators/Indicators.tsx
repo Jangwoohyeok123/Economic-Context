0f9fc3778883e3397ea9191abd2519d1dd450b45
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-03-08 16:44:16 +0900

    feat: Indicator tab delete 버튼 구현

--- src/components/indicators/Indicators.tsx
+++ src/components/indicators/Indicators.tsx
@@ -1,23 +1,43 @@
+import app from '@/firebase/firebaseConfig';
 import clsx from 'clsx';
 import styles from './Indicators.module.scss';
-import { useState } from 'react';
-import IndicatorCard from '../indicatorCard/IndicatorCard';
-import MakeConfirmModal from '../modals/makeConfirmModal/MakeConfirmModal';
-import ChartModal from '../modals/chartModal/ChartModal';
-import { useQuery } from '@tanstack/react-query';
 import queryKey from '@/const/queryKey';
-import app from '@/firebase/firebaseConfig';
-import { getDatabase, get, ref } from 'firebase/database';
+import ChartModal from '../modals/chartModal/ChartModal';
+import { useState } from 'react';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import IndicatorCard from '../indicatorCard/IndicatorCard';
+import { Indicator } from '@/types/dbInterface';
+import MakeConfirmModal from '../modals/makeConfirmModal/MakeConfirmModal';
+import { getDatabase, get, ref, remove } from 'firebase/database';
 import { changeNameToCategoryId } from '@/utils/changeNameToCategoryId';
 
-type Favorite = {
-	title: string;
-	seriesId: string;
-	categoryId: number;
+export const deleteFavorite = async (userId: number, seriresId: string) => {
+	const db = getDatabase(app); // firebase Instance 갖고오기
+	const favoriteDocRef = ref(db, `/user/favorite/${userId}`); // 경로 생성
+	const snapshot = await get(favoriteDocRef); // get 한 결과를 snapshot 으로 전달받기 get 함수는 then 처리까지 스스로 처리함
+
+	// seriesId 가 일치하는 snapshot key 를 찾는다.
+	if (snapshot.exists()) {
+		let targetKeyToRemove = null;
+		snapshot.forEach(childSnapshot => {
+			const favorite = childSnapshot.val();
+			console.log(childSnapshot.key);
+
+			if (favorite.seriesId === seriresId) {
+				targetKeyToRemove = childSnapshot.key;
+			}
+		});
+
+		if (targetKeyToRemove) {
+			const refToRemove = ref(db, `user/favorite/${userId}/${targetKeyToRemove}`);
+			await remove(refToRemove);
+		}
+	}
 };
 
 export default function Indicators() {
 	const userId = 1;
+	const queryClient = useQueryClient();
 	const categoryNames = ['interest', 'exchange', 'consume', 'production'];
 	const [categoryIndex, setCategoryIndex] = useState(0);
 	const [isChartModalOpen, setIsChartModalOpen] = useState(false);
@@ -29,7 +49,7 @@ export default function Indicators() {
 			const db = getDatabase(app);
 			const favoriteDocRef = ref(db, `/user/favorite/${userId}`);
 			const snapshot = await get(favoriteDocRef);
-			const favoritesArray: Favorite[] = [];
+			const favoritesArray: Indicator[] = [];
 
 			if (snapshot.exists()) {
 				snapshot.forEach(childSnapshot => {
@@ -38,11 +58,24 @@ export default function Indicators() {
 			}
 
 			return favoritesArray;
+		},
+		staleTime: 1 * 60 * 1000 // 1분
+	});
+
+	const deleteMutation = useMutation({
+		mutationFn: ({ userId, seriesId }: { userId: number; seriesId: string }) => deleteFavorite(userId, seriesId),
+		onSuccess(data) {
+			console.log(data);
+			alert('mutate 성공');
+			queryClient.invalidateQueries({ queryKey: [queryKey.favorite, userId] }); // 화면을 최신상태로 유지하는 방법
+		},
+		onError(err) {
+			console.error(err);
 		}
 	});
 
 	// 범용성있게 함수이름을 failterDataSet 라고 정의할까요?
-	const filterFavoriteByCategoryId = (favorites: Favorite[], categoryId: number) => {
+	const filterFavoriteByCategoryId = (favorites: Indicator[], categoryId: number) => {
 		return favorites.filter(favorite => favorite.categoryId === categoryId);
 	};
 
@@ -74,6 +107,7 @@ export default function Indicators() {
 				{isSuccess &&
 					filterFavoriteByCategoryId(favorites, changeNameToCategoryId(categoryNames[categoryIndex])).map(
 						(favorite, idx) => {
+							const seriesId = favorite.seriesId;
 							return (
 								<IndicatorCard
 									isChartModalOpen={isChartModalOpen}
@@ -84,7 +118,9 @@ export default function Indicators() {
 									pageType='dashboard'
 									title={favorite.title}
 									leftButtonContent='delete'
-									leftButtonHandler={() => {}}
+									leftButtonHandler={() => {
+										deleteMutation.mutate({ userId, seriesId });
+									}}
 									rightButtonContent='check'
 									rightButtonHandler={() => {}}></IndicatorCard>
 							);
