259bfe6a404c267f5f7d2893b6c23ee0bc2e6983
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-03-11 17:46:03 +0900

    feat: classNames Index 와 buttons map index 가 일치하면 styles.on 부여
    - 컴포넌트의 Index 와 ui 를 그리는 map 함수의 index 가 같으면 styles.on 부여하기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx';
-import User from '@/types/userInterface';
+import User, { Indicator, IndicatorWithIsActive } from '@/types/userInterface';
 import Image from 'next/image';
 import axios from 'axios';
 import styles from './Home.module.scss';
@@ -10,30 +10,49 @@ import { useQuery } from '@tanstack/react-query';
 import IndicatorCard from '@/components/cards/indicatorCard/IndicatorCard';
 import { useRouter } from 'next/router';
 import const_queryKey from '@/const/queryKey';
+import { getFavorite, getFavorites } from '@/backendApi/user';
 import { roboto, poppins } from './_app';
 import { Category, Seriess } from '@/types/fredInterface';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { changeNameToCategoryId } from '@/utils/changeNameToCategoryId';
 import { useDispatch, useSelector } from 'react-redux';
 import { getIndicator, getIndicators, getChartData } from '@/backendApi/fred';
-import const_categoryTypes from '@/const/categoryId';
-import const_categoryColor from '@/const/categoryColor';
 
 const DynamicAlertModal = dynamic(() => import('@/components/modals/alertModal/AlertModal'), { ssr: false });
 const DynamicChartModal = dynamic(() => import('@/components/modals/chartModal/ChartModal'), { ssr: false });
+
+/* 
+	fetching 순서 == enhancedCategory useEffect 과정
+	초기
+	1. category 데이터를 fred 에서 갖고온다. 
+	2. category 데이터에 isActive false 를 부여한다.
+	3. favorites 를 back 에서 갖고온다.
+	4. category 데이터 중 favorites 데이터와 일치하는 데이터는 isActive on 을 부여한다.
+
+	button 은 enhancedCategory 를 처리한다.
+	클릭하면 enhancedCategory 에서 isActive 를 toggle 한다.
+	클릭하면 modify 함수를 호출하여 isActive 가 false 면 save 한다.
+	클릭하면 modify 함수를 호출하여 isActive 가 true 면 delete 한다.
+*/
 
 export default function Pages({ interest }: { interest: Category }) {
 	const user = useSelector((state: Store) => state.user);
 	const router = useRouter();
 	const dispatch = useDispatch();
-	// const refCategoryButtons = useRef<HTMLButtonElement>([]);
 	const categoryNames = ['interest', 'exchange', 'production', 'consume'];
 	const [categoryIndex, setCategoryIndex] = useState(0);
-	const [IsAlertModalOpen, setIsAlertModalOpen] = useState(false);
+	const [isAlertModalOpen, setIsAlertModalOpen] = useState(false);
 	const [isChartModalOpen, setIsChartModalOpen] = useState(false);
-	const { data: category, isSuccess } = useQuery({
+	const [enhancedCategory, setEnhancedCategory] = useState([]);
+	const { data: category, isSuccess: isCategoryExist } = useQuery({
 		queryKey: [const_queryKey.category, changeNameToCategoryId(categoryNames[categoryIndex])],
-		queryFn: () => getIndicators(changeNameToCategoryId(categoryNames[categoryIndex]))
+		queryFn: () => getIndicators(changeNameToCategoryId(categoryNames[categoryIndex])),
+		initialData: interest.seriess
+	});
+
+	const { data: favorite, isSuccess: isFavoriteExist } = useQuery({
+		queryKey: [const_queryKey.favorite, user.id],
+		queryFn: () => getFavorite(user.id, changeNameToCategoryId(categoryNames[categoryIndex]))
 	});
 
 	const setJwtAndUserData = (authCode: string) => {
@@ -55,59 +74,51 @@ export default function Pages({ interest }: { interest: Category }) {
 		}
 	};
 
-	// const categoryTabHover = (categoryId: number, idx: number) => {
-	// 	let backgroundColor: string = const_categoryColor.interest;
-	// 	if (categoryId === const_categoryTypes.interest) backgroundColor = const_categoryColor.interest;
-	// 	else if (categoryId === const_categoryTypes.exchange) backgroundColor = const_categoryColor.exchange;
-	// 	else if (categoryId === const_categoryTypes.production) backgroundColor = const_categoryColor.production;
-	// 	else if (categoryId === const_categoryTypes.consume) backgroundColor = const_categoryColor.consume;
-
-	// 	const mouseEnter = () => {
-	// 		if (refCategoryButtons.current[idx]) {
-	// 			refCategoryButtons.current[idx].style.backgroundColor = backgroundColor;
-	// 		}
-	// 	};
-	// 	const mouseLeave = () => {
-	// 		if (refCategoryButtons.current[idx]) {
-	// 			refCategoryButtons.current[idx].style.backgroundColor = '#efefef';
-	// 		}
-	// 	};
-
-	// 	return { mouseEnter, mouseLeave };
-	// };
+	const buttonToggle = (seriesId: string, categoryId: number, indicatorIndex: number) => {};
 
 	useEffect(() => {
 		const authCode = router.query.code;
 		if (authCode) setJwtAndUserData(authCode as string);
 	}, [router.query]);
 
-	//
+	useEffect(() => {
+		if (isCategoryExist && category && isFavoriteExist && favorite) {
+			const categoryWithIsActive = category.map((item: Seriess) => ({
+				...item,
+				isActive: false
+			}));
+
+			favorite.forEach((favoriteIndicator: Indicator) => {
+				categoryWithIsActive.forEach((categoryIndicator: IndicatorWithIsActive) => {
+					if (favoriteIndicator.seriesId === categoryIndicator.seriesId) categoryIndicator.isActive = true;
+				});
+			});
+			setEnhancedCategory(categoryWithIsActive);
+		}
+	}, [category, isCategoryExist, isFavoriteExist, favorite]);
+
 	return (
 		<>
 			<main className={clsx(styles.Home, poppins.variable, roboto.variable)}>
 				<div className={clsx(styles.mainImage)}>
 					<Image src='/mainImage.jpg' alt='mainImage' layout='fill' objectFit='cover' />
 				</div>
-
+				{/* 현재 카테고리위치를 state 처리하고, button 중 index 와 일치하면 on 을 준다. */}
 				<div className={clsx(styles.categoryNames)}>
-					{categoryNames.map((categoryName, idx) => {
-						// const { mouseEnter, mouseLeave } = categoryTabHover(changeNameToCategoryId(categoryName), idx);
+					{categoryNames.map((_, idx) => {
 						return (
 							<button
-								// ref={el => (refCategoryButtons.current[idx] = el)}
+								className={clsx(categoryIndex === idx ? styles.on : '')}
 								key={idx}
-								onClick={() => setCategoryIndex(idx)}
-								// onMouseEnter={mouseEnter}
-								// onMouseLeave={mouseLeave}
-							>
+								onClick={() => setCategoryIndex(idx)}>
 								{categoryNames[idx]}
 							</button>
 						);
 					})}
 				</div>
 				<figure className={clsx(styles.category)}>
-					{isSuccess
-						? category.map((series: Seriess, idx: number) => {
+					{isCategoryExist
+						? enhancedCategory.map((series: Seriess, idx: number) => {
 								let notes;
 								const title = series.title;
 								const seriesId = series.id;
@@ -136,7 +147,10 @@ export default function Pages({ interest }: { interest: Category }) {
 												<p>This indicator does not have information about the indicator description.</p>
 											)}
 										</div>
-										<button>save</button>
+										<button
+											onClick={() => buttonToggle(seriesId, changeNameToCategoryId(categoryNames[categoryIndex]), idx)}>
+											save
+										</button>
 									</IndicatorCard>
 								);
 						  })
@@ -144,7 +158,7 @@ export default function Pages({ interest }: { interest: Category }) {
 				</figure>
 			</main>
 			<DynamicAlertModal
-				isModalOpen={IsAlertModalOpen}
+				isModalOpen={isAlertModalOpen}
 				setIsModalOpen={setIsAlertModalOpen}
 				size='small'
 				header='You need to login!'
@@ -159,8 +173,7 @@ export default function Pages({ interest }: { interest: Category }) {
 	);
 }
 
-// server 쪽에서 popularity 기준으로 sort 를 진행하고 전달하고 싶은데 ...
-// promise.all 적용실패
+// 초기 애플리케이션 실행시에만 서버사이드 렌더링을 진행
 export async function getServerSideProps() {
 	const baseUrl = 'https://api.stlouisfed.org/fred/';
 	const fetchInterestCategory = await fetch(
@@ -168,31 +181,9 @@ export async function getServerSideProps() {
 	);
 	const interest = await fetchInterestCategory.json();
 
-	// const fetchExchangeCategory = await fetch(
-	// 	`${baseUrl}category/series?category_id=94&api_key=${process.env.NEXT_PUBLIC_FREDKEY}&file_type=json`
-	// );
-	// const exchange = await fetchExchangeCategory.json();
-
-	// const fetchConsumeCategory = await fetch(
-	// 	`${baseUrl}category/series?category_id=9&api_key=${process.env.NEXT_PUBLIC_FREDKEY}&file_type=json`
-	// );
-	// const consume = await fetchConsumeCategory.json();
-
-	// const fetchProductionCategory = await fetch(
-	// 	`${baseUrl}category/series?category_id=31&api_key=${process.env.NEXT_PUBLIC_FREDKEY}&file_type=json`
-	// );
-	// const production = await fetchProductionCategory.json();
-
 	return {
-		// 4개를 한 번에 호출해야하는지 고민해볼 것
-		// prefetching? hover 시 fetcing 이 일어나는 기능이 있음
-		// a fethcing 하고 bcd 는 prefetcging 이용할 수 있음 promise.all 과 고민해볼 것
-		// props 는 json'Category이름' 꼴로 전송해야한다. 화면에 json 이후 글자가 표기되기 때문이다.
 		props: {
 			interest
-			// exchange,
-			// consume,
-			// production
 		}
 	};
 }
