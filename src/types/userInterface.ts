862567613cfd3acafc0fc297065d2071fed8655a
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-03-11 17:44:51 +0900

    feat: Indicator, IndicatorWithAcite 타입 추가하기
    - isActive 추가함

--- src/types/userInterface.ts
+++ src/types/userInterface.ts
@@ -1,11 +1,24 @@
 interface User {
 	isLogin: boolean;
-	id: number | string;
+	id: string;
 	google_id: string;
 	email: string;
 	picture_url: string;
 	favorite_indicators: string[];
 	createAt: Date;
+}
+
+export interface Indicator {
+	title: string;
+	seriesId: string;
+	categoryId: number;
+}
+
+export interface IndicatorWithIsActive {
+	title: string;
+	seriesId: string;
+	categoryId: number;
+	isActive: boolean;
 }
 
 export default User;
