42b0567420ef24ec0c1b02a68da4006de9eff70e
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-03-13 11:33:44 +0900

    refator: Dashboard 페이지가 사용하는 component 명 rename
    - Indicators 는 Indicator 의 집합을 의미하는데 너무 범용적이어서 tab 메뉴를 뜻하는 IndicatorTab 으로 변경
    - 마찬가지로 MyContext 에서 MyContextTab 으로 변경

--- src/components/myContext/MyContext.tsx
+++ src/components/myContext/MyContext.tsx
@@ -1,6 +1,6 @@
 import clsx from 'clsx';
 import styles from './MyContext.module.scss';
 
-export default function MyContext() {
+export default function MyContextTab() {
 	return <section className={clsx(styles.MyContext)}></section>;
 }
