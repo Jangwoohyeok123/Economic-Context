993ce24e667784f30e72afa69ff085472b798d87
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-03-14 16:26:20 +0900

    refactor: fredAPI Type optional 및 required 분리

--- src/types/fredType.ts
+++ src/types/fredType.ts
@@ -1,19 +1,19 @@
 // util 타입을 좀 사용해보자 pick ommit
 export type Seriess_Type = {
+	id: string;
+	title: string;
+	notes?: string;
+	observation_start?: string;
+	observation_end?: string;
 	frequency?: string;
 	frequency_short?: string;
 	group_popularity?: number;
-	id: string;
-	last_updated?: string;
-	notes?: string;
-	observation_end?: string;
-	observation_start?: string;
 	popularity?: number;
-	realtime_end?: string;
+	last_updated?: string;
 	realtime_start?: string;
+	realtime_end?: string;
 	seasonal_adjustment?: string;
 	seasonal_adjustment_short?: string;
-	title: string;
 	units?: string;
 	units_short?: string;
 };
