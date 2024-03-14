be70b1df3876029e7e51682e823c063ac2a27e66
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-03-14 09:43:40 +0900

    feat: more 페이지 promise 로 useEffect 작업 및 스타일링 진행중
    - promise 를 사용해 useEffect 를 구성했다.
    - async 함수는 즉시실행함수를 사용해야 하는데 개인적으로 promise 가 더 가독성이 좋아보였다.

--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -2,10 +2,10 @@ import clsx from 'clsx';
 import styles from './CategoryId.module.scss';
 import LineChart from '@/components/charts/line/LineChart';
 import { useRouter } from 'next/router';
-import { poppins, roboto } from './_app';
 import { useEffect, useState } from 'react';
-import searchDotAndSetValue from '@/utils/searchDotAndSetValue';
-import { getChartData } from '@/backendApi/fred';
+import { getChartData, getIndicator } from '@/backendApi/fred';
+import { poppins, roboto } from './_app';
+import { Seriess_Type } from '@/types/fredType';
 
 interface DataItem {
 	date: Date;
@@ -14,11 +14,63 @@ interface DataItem {
 
 export default function CategoryId() {
 	const router = useRouter();
-	const { id: seriesId } = router.query;
+	const { id: seriesId, cleandTitle } = router.query;
+	const [chartDatas, setChartDatas] = useState<DataItem[]>([]);
+	const [indicators, setIndicators] = useState<Seriess_Type>({
+		id: '',
+		title: '',
+		notes: '',
+		observation_start: '',
+		observation_end: ''
+	});
 
 	useEffect(() => {
-		getChartData(seriesId as string).then(chartData => console.log(chartData));
-	}, []);
+		getChartData(seriesId as string)
+			.then(chartDatas => {
+				const { dataArray } = chartDatas;
+				setChartDatas(dataArray);
+			})
+			.catch(err => {
+				console.error(err.message);
+			});
 
-	return <main className={clsx(styles.CategoryId, poppins.variable, roboto.variable)}></main>;
+		getIndicator(seriesId as string).then(indicator => {
+			const { seriesId: id, notes, observation_start, observation_end } = indicator;
+			setIndicators(prev => ({
+				...prev,
+				seriesId: id,
+				cleandTitle, // Indicator card 가 query parmeter 로 전달한 값입니다.
+				notes: notes ?? '',
+				observation_start,
+				observation_end
+			}));
+		});
+	}, [seriesId]);
+
+	return (
+		<main className={clsx(styles.CategoryId, poppins.variable, roboto.variable)}>
+			<div>asd</div>
+			{chartDatas.length && indicators && <LineChart indicators={indicators} values={chartDatas} />}
+		</main>
+	);
 }
+
+/* promise 와 async/await 의 차이점
+	[ async-await ] => '변수선언 = 비동기함수결과' 의 직관적인 코드 작성가능하다.
+	try {
+		const { realtime_start, realtime_end, dataArray } = await getChartDataseriesId as string);
+		return { realtime_start, realtime_end, dataArray };
+	} catch (err) {
+		console.error()
+		return 'fallback value';
+	}
+
+	[ promise ] => 'promise 객체의 then 에서 변수선언 후 처리하는 방식으로 상대적으로 직관적이지 않다.'
+	getChartData(seriesId as string).then((result) => {
+		const { realtime_start, realtime_end, dataArray } = result;
+	})
+
+	[ useEffect 에서 async vs promise ]
+	promise 가 개인적으로 맘에드는 이유는 useEffect 에서 await 를 쓰기 위해서는 async() => {}(); 꼴의
+	비동기 즉시실행함수를 사용해야하기 때문에 프로미스를 생각했다.
+*/
