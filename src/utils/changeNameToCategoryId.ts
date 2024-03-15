31cfac24aba137be5ab61aacaafe55763d0abbb8
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-03-15 15:31:10 +0900

    refactor: categoryNames 배열을 app 으로 이동시켜서 export 로 자동화처리

--- src/utils/changeNameToCategoryId.ts
+++ src/utils/changeNameToCategoryId.ts
@@ -2,20 +2,20 @@ import const_categoryTypes from '@/const/categoryId';
 
 export const changeNameToCategoryId = (categoryName: string): number => {
 	let type = const_categoryTypes.interest;
-	if (categoryName === 'interest') return (type = const_categoryTypes.interest);
-	if (categoryName === 'exchange') return (type = const_categoryTypes.exchange);
-	if (categoryName === 'production') return (type = const_categoryTypes.production);
-	if (categoryName === 'consume') return (type = const_categoryTypes.consume);
+	if (categoryName === 'Interest') return (type = const_categoryTypes.interest);
+	if (categoryName === 'Exchange') return (type = const_categoryTypes.exchange);
+	if (categoryName === 'Production') return (type = const_categoryTypes.production);
+	if (categoryName === 'Consume') return (type = const_categoryTypes.consume);
 
 	return type;
 };
 
 export const changeCategoryIdToName = (categoryType: number): string => {
-	let categoryName = 'interest';
-	if (categoryType === const_categoryTypes.interest) return (categoryName = 'interest');
-	if (categoryType === const_categoryTypes.exchange) return (categoryName = 'exchange');
-	if (categoryType === const_categoryTypes.production) return (categoryName = 'production');
-	if (categoryType === const_categoryTypes.consume) return (categoryName = 'consume');
+	let categoryName = 'Interest';
+	if (categoryType === const_categoryTypes.interest) return (categoryName = 'Interest');
+	if (categoryType === const_categoryTypes.exchange) return (categoryName = 'Exchange');
+	if (categoryType === const_categoryTypes.production) return (categoryName = 'Production');
+	if (categoryType === const_categoryTypes.consume) return (categoryName = 'Consume');
 
 	return categoryName;
 };
