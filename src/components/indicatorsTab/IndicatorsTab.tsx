691c851e38c084cfb390d994495d990628e2d6bc
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-03-16 19:14:42 +0900

    feat: 첫 화면 렌더링이 안됨

--- src/components/indicatorsTab/IndicatorsTab.tsx
+++ src/components/indicatorsTab/IndicatorsTab.tsx
@@ -1,9 +1,9 @@
 import clsx from 'clsx';
 import styles from './IndicatorsTab.module.scss';
 import { Store } from '@/types/reduxType';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import ReactPaginate from 'react-paginate';
-import { Indicator } from '@/types/userType';
+import { Indicator, IndicatorWithIsPick } from '@/types/userType';
 import IndicatorCard from '../cards/indicatorCard/IndicatorCard';
 import { useSelector } from 'react-redux';
 import BubblePopButton from '../bubblePopButton/BubblePopButton';
@@ -11,6 +11,8 @@ import useFavoriteQuery from '@/hooks/useFavoriteQuery';
 import { categoryNames } from '@/pages/_app';
 import useFavoriteMutation from '@/hooks/useFavoriteMutation';
 import { changeCategoryIdToName, changeNameToCategoryId } from '@/utils/changeNameToCategoryId';
+import { cleanString } from '@/utils/cleanString';
+import MakeContextModal from '../modals/makeContextModal/MakeContextModal';
 
 /** [key: string] 는 key 를 여러개 갖을 수 있고 값은 Indicator[] 을 갖는다는 의미의 type 이다. */
 type PickIndicators = {
@@ -27,19 +29,19 @@ export default function IndicatorsTab() {
 	const [categoryIndex, setCategoryIndex] = useState(0);
 	const categoryId = changeNameToCategoryId(categoryNames[categoryIndex]);
 	const [currentPage, setCurrentPage] = useState(0);
-	const [isOpenConfirmContext, setIsOpenConfirmContext] = useState(false);
-	const { selectedFavorites } = useFavoriteQuery(categoryId);
+	const { allFavorites } = useFavoriteQuery();
 	const { deleteFavoriteMutationAll } = useFavoriteMutation();
-	const [pickIndicators, setPickIndicators] = useState<PickIndicators>({});
+	const [isMakeOpen, setIsMakeOpen] = useState(false);
 
-	useEffect(() => {
-		setPickIndicators(
-			categoryNames.reduce((acc: PickIndicators, name: string) => {
-				acc[name] = [];
-				return acc;
-			}, {} as PickIndicators)
-		);
-	}, []);
+	const [favorites, setFavorites] = useState(
+		allFavorites?.map(favorite => {
+			return {
+				...favorite,
+				isPick: false
+			};
+		})
+	);
+	const curFavorites = favorites?.filter(favorite => favorite.categoryId == categoryId);
 
 	return (
 		<div className={clsx(styles.IndicatorsTab)}>
@@ -56,39 +58,43 @@ export default function IndicatorsTab() {
 				})}
 			</nav>
 			<section className={clsx(styles.favorites)}>
-				{selectedFavorites
+				{curFavorites
 					?.slice(currentPage * itemsPerPage, (currentPage + 1) * itemsPerPage)
-					.map((favorite: Indicator, index: number) => {
-						const { title, seriesId, categoryId } = favorite;
+					.map((favorite: IndicatorWithIsPick, index: number) => {
+						const { title, seriesId, notes, categoryId, observation_end, observation_start, isPick } = favorite;
 						return (
 							<IndicatorCard
 								key={index}
 								title={title}
 								seriesId={seriesId}
 								categoryId={categoryId}
-								observation_end=' favorite 요소'
-								observation_start='백엔드 요청사항: favorite 요소'
+								observation_end={observation_end}
+								observation_start={observation_start}
+								notes={notes}
 								className={clsx(styles.indicatorCardByTab)}>
+								<p>{notes ? notes.slice(300) : 'This Indicator is not have notes'}</p>
 								<div className={styles.buttons}>
 									<BubblePopButton
 										clickHandler={() => deleteFavoriteMutationAll?.mutate({ userId: user.id, seriesId })}>
 										delete
 									</BubblePopButton>
 									<BubblePopButton
+										className={isPick ? clsx(styles.on) : ''}
 										clickHandler={() => {
-											const curCategoryName = changeCategoryIdToName(categoryId);
-											let isActive = false;
-											pickIndicators.curCategoryName.forEach((el ) => {
-												if(el.seriesId === seriesId) return true;
-											})
-											if (pickIndicators.curCategoryName)
-											// pickIndicators 의 현 category 에서 seriesId 가 이 카드의 seriesId 가 존재하면
-											// 클릭할 때 그 배열에 값이 있는지 확인한다.
-											// 없다면 state 추가
-											// 있따면 삭제
-											// 현 sereisId 가 pickIndicators 에 존재한다면 styles.on 아니면 off
+											const updated = favorites.map(favorite => {
+												if (favorite.seriesId === seriesId) {
+													return {
+														...favorite,
+														isPick: !favorite.isPick
+													};
+												}
+
+												return favorite;
+											});
+
+											setFavorites(updated);
 										}}>
-										pick
+										{isPick ? 'unpick' : 'pick'}
 									</BubblePopButton>
 								</div>
 							</IndicatorCard>
@@ -97,9 +103,9 @@ export default function IndicatorsTab() {
 			</section>
 			<footer>
 				<span className={clsx(styles.item)}></span>
-				{selectedFavorites && (
+				{curFavorites && (
 					<ReactPaginate
-						pageCount={Math.ceil(selectedFavorites.length / itemsPerPage)}
+						pageCount={Math.ceil(curFavorites.length / itemsPerPage)}
 						previousAriaLabel='prev card'
 						nextAriaLabel='next card'
 						previousLabel='Prev'
@@ -112,19 +118,20 @@ export default function IndicatorsTab() {
 						forcePage={currentPage}
 						activeClassName={styles.paginationActive}
 						previousClassName={currentPage === 0 ? styles.disabled : ''}
-						nextClassName={currentPage === Math.ceil(selectedFavorites.length / itemsPerPage) ? styles.disabled : ''}
+						nextClassName={currentPage === Math.ceil(curFavorites.length / itemsPerPage) ? styles.disabled : ''}
 						disabledClassName={styles.disabled}
 					/>
 				)}
 				<div className={clsx(styles.item, styles.buttonWrap)}>
 					<button
 						onClick={() => {
-							setIsOpenConfirmContext(true);
+							setIsMakeOpen(true);
 						}}>
 						Make Context
 					</button>
 				</div>
 			</footer>
+			<MakeContextModal favorites={allFavorites} isModalOpen={isMakeOpen} setIsModalOpen={setIsMakeOpen} />
 		</div>
 	);
 }
