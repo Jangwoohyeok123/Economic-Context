447b21ce598a59859c6bd103c0e7db49cabd36c8
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-03-16 09:08:21 +0900

    feat: IndicatorTab 을 구성하는 카드의 버튼을 BubblePopButton 컴포넌트로 교체

--- src/components/indicatorsTab/IndicatorsTab.tsx
+++ src/components/indicatorsTab/IndicatorsTab.tsx
@@ -1,26 +1,45 @@
 import clsx from 'clsx';
 import styles from './IndicatorsTab.module.scss';
-import { useState } from 'react';
+import { Store } from '@/types/reduxType';
+import { useEffect, useState } from 'react';
+import ReactPaginate from 'react-paginate';
 import { Indicator } from '@/types/userType';
 import IndicatorCard from '../cards/indicatorCard/IndicatorCard';
+import { useSelector } from 'react-redux';
+import BubblePopButton from '../bubblePopButton/BubblePopButton';
 import useFavoriteQuery from '@/hooks/useFavoriteQuery';
 import { categoryNames } from '@/pages/_app';
 import useFavoriteMutation from '@/hooks/useFavoriteMutation';
-import { changeNameToCategoryId } from '@/utils/changeNameToCategoryId';
-import ReactPaginate from 'react-paginate';
+import { changeCategoryIdToName, changeNameToCategoryId } from '@/utils/changeNameToCategoryId';
+
+/** [key: string] 는 key 를 여러개 갖을 수 있고 값은 Indicator[] 을 갖는다는 의미의 type 이다. */
+type PickIndicators = {
+	[key: string]: Indicator[];
+};
 
 /**
 - IndicatorsTab 을 클릭하면 첫 selectedFavorite 이 페칭된다.
 - categoryIndex 가 바뀌면 useFavoriteQuery 에 의해서 페칭이 되는 구조다.
 */
 export default function IndicatorsTab() {
+	const user = useSelector((state: Store) => state.user);
+	const itemsPerPage = 3;
 	const [categoryIndex, setCategoryIndex] = useState(0);
 	const categoryId = changeNameToCategoryId(categoryNames[categoryIndex]);
-	const [isOpenConfirmContext, setIsOpenConfirmContext] = useState(false);
-	const { selectedFavorites, isSelectedFavoritesExist } = useFavoriteQuery(categoryId);
-	const { addFavoriteMutationAll, deleteFavoriteMutationAll } = useFavoriteMutation();
-	const itemsPerPage = 3;
 	const [currentPage, setCurrentPage] = useState(0);
+	const [isOpenConfirmContext, setIsOpenConfirmContext] = useState(false);
+	const { selectedFavorites } = useFavoriteQuery(categoryId);
+	const { deleteFavoriteMutationAll } = useFavoriteMutation();
+	const [pickIndicators, setPickIndicators] = useState<PickIndicators>({});
+
+	useEffect(() => {
+		setPickIndicators(
+			categoryNames.reduce((acc: PickIndicators, name: string) => {
+				acc[name] = [];
+				return acc;
+			}, {} as PickIndicators)
+		);
+	}, []);
 
 	return (
 		<div className={clsx(styles.IndicatorsTab)}>
@@ -49,8 +68,29 @@ export default function IndicatorsTab() {
 								categoryId={categoryId}
 								observation_end=' favorite 요소'
 								observation_start='백엔드 요청사항: favorite 요소'
-								className={clsx(styles.IndicatorCard)}>
-								<div>asd</div>
+								className={clsx(styles.indicatorCardByTab)}>
+								<div className={styles.buttons}>
+									<BubblePopButton
+										clickHandler={() => deleteFavoriteMutationAll?.mutate({ userId: user.id, seriesId })}>
+										delete
+									</BubblePopButton>
+									<BubblePopButton
+										clickHandler={() => {
+											const curCategoryName = changeCategoryIdToName(categoryId);
+											let isActive = false;
+											pickIndicators.curCategoryName.forEach((el ) => {
+												if(el.seriesId === seriesId) return true;
+											})
+											if (pickIndicators.curCategoryName)
+											// pickIndicators 의 현 category 에서 seriesId 가 이 카드의 seriesId 가 존재하면
+											// 클릭할 때 그 배열에 값이 있는지 확인한다.
+											// 없다면 state 추가
+											// 있따면 삭제
+											// 현 sereisId 가 pickIndicators 에 존재한다면 styles.on 아니면 off
+										}}>
+										pick
+									</BubblePopButton>
+								</div>
 							</IndicatorCard>
 						);
 					})}
