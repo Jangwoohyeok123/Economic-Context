ba06801c23101c9b92761520d0cf3f408f0cd134
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-03-20 12:17:33 +0900

    feat: AllContext 추가
    - MyContext 탭을 누르면 실행되는 컴포넌트

---
+++ src/components/allContexts/AllContexts.tsx
@@ -0,0 +1,22 @@
+import clsx from 'clsx';
+import styles from './AllContexts.module.scss';
+import { useQuery } from '@tanstack/react-query';
+import const_queryKey from '@/const/queryKey';
+import { getAllContexts } from '@/backendApi/user';
+import { ContextType } from '@/types/userType';
+import { useSelector } from 'react-redux';
+import { Store } from '@/types/reduxType';
+
+export default function AllContexts() {
+	const userId = useSelector((store: Store) => store.user.id);
+	const { data: allContexts, isLoading } = useQuery<ContextType[]>({
+		queryKey: [const_queryKey.context, 'all'],
+		queryFn: () => getAllContexts(userId)
+	});
+
+	if (isLoading) {
+		<div>Loading...</div>;
+	}
+
+	return <section className={clsx(styles.AllContexts)}></section>;
+}
