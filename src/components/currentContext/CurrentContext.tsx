fe0de7a29596dfbd0a033270a2dcdb83cb00540a
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-03-20 12:19:08 +0900

    refactor: CurrentContext 추가
    - MyContext 하위 탭을 누를 때 실행될 component

---
+++ src/components/currentContext/CurrentContext.tsx
@@ -0,0 +1,23 @@
+import clsx from 'clsx';
+import styles from './CurrentContext.module.scss';
+import const_queryKey from '@/const/queryKey';
+import { getContext, getContextIdsWithNames } from '@/backendApi/user';
+import { Store } from '@/types/reduxType';
+import { useSelector } from 'react-redux';
+import { useQuery } from '@tanstack/react-query';
+import { ContextType } from '@/types/userType';
+
+interface CurrentContextProps {
+	currentContextId: number;
+}
+
+export default function CurrentContext({ currentContextId }: CurrentContextProps) {
+	const { data: context, isLoading } = useQuery<ContextType>({
+		queryKey: [const_queryKey.context, currentContextId],
+		queryFn: () => getContext(currentContextId)
+	});
+
+	if (isLoading) return <div>Loading...</div>;
+
+	return <section className={clsx(styles.CurrentContext)}></section>;
+}
