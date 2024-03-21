d99f410163c37a6df60200e12e730b07a5f0a27c
aajy <aajy.000@gmail.com>
2024-03-21 23:54:24 +0900

    feature: Journal컴포넌트 내 Journal이 없을 때 처리
    - useQuery 의 isJournalLoading을 활용한 조건문 처리로 저널이 없을 때 "작성된 Journal이 없습니다. 위의 칸을 채워 Journal을 등록 해보세요!" section 추가
    
    - 핸들러 함수 첫문자 대문자 ->소문자로 수정
    - Journal input placeholder 추가
    - JournalList 사이 padding 추가

--- src/components/journal/Journal.tsx
+++ src/components/journal/Journal.tsx
@@ -1,6 +1,6 @@
 import clsx from 'clsx';
 import styles from './Journal.module.scss';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Journal, JournalResponseData } from '@/types/userType';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import const_queryKey from '@/const/queryKey';
@@ -17,27 +17,9 @@ interface JournalProps {
 export default function Journal({ contextId }: JournalProps) {
 	const userId = useSelector((state: Store) => state.user.id);
 	const [isWrite, setIsWrite] = useState(false);
+	const [journalDataParams, setJournalDataParams] = useState({ title: '', body: '' });
 	const queryClient = useQueryClient();
 
-	const [journalDataParams, setJournalDataParams] = useState({ title: '', body: '' });
-
-	const ToggleJournalFormButton = (e: React.FormEvent<HTMLSpanElement>) => {
-		e.preventDefault();
-		setIsWrite(prev => !prev);
-	};
-	const ChangeJournalInputData = (e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) => {
-		e.preventDefault();
-		const { name, value } = e.target; // e.target에서 name과 value를 추출
-		const newParams: Journal = {
-			...journalDataParams,
-			[name]: value
-		};
-		setJournalDataParams(newParams);
-	};
-	const registJournal = (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		makeJournal();
-	};
 	const addJournalMutation = useMutation({
 		mutationFn: ({
 			userId,
@@ -52,13 +34,13 @@ export default function Journal({ contextId }: JournalProps) {
 			queryClient.invalidateQueries({
 				queryKey: [const_queryKey.journal, contextId]
 			});
-
 			alert('add 성공');
 		},
 		onError(error) {
 			console.error(error);
 		}
 	});
+
 	const makeJournal = () => {
 		if (journalDataParams.body) {
 			addJournalMutation.mutate({ userId, contextId, journalDataParams });
@@ -73,19 +55,60 @@ export default function Journal({ contextId }: JournalProps) {
 		queryKey: [const_queryKey.journal, contextId],
 		queryFn: () => getJournal(contextId as number)
 	});
+
+	const toggleJournalFormButton = (e: React.FormEvent<HTMLSpanElement>) => {
+		e.preventDefault();
+		setIsWrite(prev => !prev);
+	};
+
+	const changeJournalInputData = (e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) => {
+		e.preventDefault();
+		const { name, value } = e.target; // e.target에서 name과 value를 추출
+		const newParams: Journal = {
+			...journalDataParams,
+			[name]: value
+		};
+		setJournalDataParams(newParams);
+	};
+
+	const registJournal = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		makeJournal();
+	};
+	useEffect(() => {
+		// journal 데이터가 있고 항목이 하나 이상 있는 경우 isOpen을 true로 설정
+		if (journal && journal.length > 0) {
+			setIsWrite(false);
+		} else {
+			setIsWrite(true); // 그렇지 않으면 false로 설정
+		}
+	}, [journal]);
+
 	return (
 		<div className={clsx(styles.Journal)}>
 			<section className={clsx(styles.JournalHeader)}>
 				<h3>Journal</h3>
-				<span className={clsx(styles.JournalFormIcon)} onClick={e => ToggleJournalFormButton(e)}>
+				<span className={clsx(styles.JournalFormIcon)} onClick={e => toggleJournalFormButton(e)}>
 					{isWrite ? <CgCloseR /> : <HiMiniPencilSquare />}
 				</span>
 			</section>
 
 			{isWrite && (
 				<form onSubmit={registJournal}>
-					<input type='text' name='title' onChange={e => ChangeJournalInputData(e)} />
-					<textarea name='body' rows={5} onChange={e => ChangeJournalInputData(e)} />
+					<label htmlFor='title'>Title</label>
+					<input
+						type='text'
+						name='title'
+						placeholder='Journal의 제목을 작성해주세요.'
+						onChange={e => changeJournalInputData(e)}
+					/>
+					<label htmlFor='body'>Body</label>
+					<textarea
+						name='body'
+						rows={5}
+						placeholder='Journal의 본문을 작성해주세요.'
+						onChange={e => changeJournalInputData(e)}
+					/>
 					<div>
 						<button>등록</button>
 					</div>
@@ -94,20 +117,27 @@ export default function Journal({ contextId }: JournalProps) {
 			<div className={clsx(styles.JournalTable)}>
 				<table>
 					<tbody>
-						{journal?.map((item: JournalResponseData, index: number) => (
-							<tr key={item.id + index}>
-								{/* 메모 */}
-								<td>
-									<div>
-										<span>{item.title}</span>
-									</div>
-									<div className={clsx(styles.description)}>
-										<span>{item.body}</span>
-										<em>{item.createdAt}</em>
-									</div>
-								</td>
-							</tr>
-						))}
+						{isJournalLoading ? (
+							<div>Loading...</div> // 로딩 중임을 알리는 메시지
+						) : journal && journal.length > 0 ? (
+							journal.map((item: JournalResponseData, index: number) => (
+								<tr key={item.id + index}>
+									<td>
+										<div>
+											<span>{item.title}</span>
+										</div>
+										<div className={clsx(styles.description)}>
+											<span>{item.body}</span>
+											<em>{item.createdAt}</em>
+										</div>
+									</td>
+								</tr>
+							))
+						) : (
+							<div className={clsx(styles.noData)}>
+								작성된 Journal이 없습니다. 위의 칸을 채워 Journal을 등록 해보세요!
+							</div> // 데이터가 없음을 알리는 메시지
+						)}
 					</tbody>
 				</table>
 			</div>
