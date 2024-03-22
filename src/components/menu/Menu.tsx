60ff0ac06037dde7314da5771a5b4625f9e816f7
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-03-22 14:01:04 +0900

    feat: menuComponent 삭제기능 추가
    =

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,13 +1,14 @@
 import clsx from 'clsx';
 import styles from './Menu.module.scss';
 import Link from 'next/link';
-import { useMutation, useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { deleteContext, getContext, getContextIdsWithNames } from '@/backendApi/user';
 import { useSelector } from 'react-redux';
 import { Store } from '@/types/reduxType';
 import const_queryKey from '@/const/queryKey';
 import { useEffect, useState } from 'react';
 import { ContextIdWithName } from '@/types/userType';
+import { RiDeleteBin6Line } from 'react-icons/ri';
 
 interface MenuProps {
 	selectedTab: string;
@@ -16,17 +17,63 @@ interface MenuProps {
 
 export default function Menu({ selectedTab, setSelectedTab }: MenuProps) {
 	const tabs = ['Indicators', 'MyContext'];
-	const [isAccordian, setIsAccordian] = useState(false);
+	const queryClient = useQueryClient();
+	const [isAccordianOpen, setIsAccordianOpen] = useState(false);
 	const userId = useSelector((state: Store) => state.user.id);
-	const { data: contextNamesWithKey, isLoading } = useQuery({
+	const { data: contextIdsWithNames, isLoading } = useQuery<ContextIdWithName[]>({
 		queryKey: [const_queryKey.context, 'names'],
 		queryFn: () => getContextIdsWithNames(userId)
 	});
 
+	const deleteContextMutation = useMutation({
+		mutationFn: (contextId: number) => deleteContext(contextId),
+		onSuccess() {
+			// if (contextIdsWithNames) {
+			// 	const { id: contextId } = contextIdsWithNames?.find(
+			// 		(context: ContextIdWithName) => context.name === selectedTab
+			// 	);
+			// 	console.log('contextIdsWithNames', contextIdsWithNames);
+			// 	queryClient.invalidateQueries({
+			// 		queryKey: [const_queryKey.context]
+			// 	});
+			// }
+			if (contextIdsWithNames) {
+				const newContextIdsWithNames = contextIdsWithNames.filter(
+					(context: ContextIdWithName) => context.name !== selectedTab
+				);
+				queryClient.setQueryData([const_queryKey.context, 'names'], newContextIdsWithNames); // 내일 정리하기
+				queryClient.invalidateQueries({
+					queryKey: [const_queryKey.context, 'names']
+				});
+
+				if (contextIdsWithNames.length >= 2) {
+					// 왜 -1 ?
+					const currentIndex = contextIdsWithNames.findIndex((context: ContextIdWithName) => {
+						return context.name.trim() === selectedTab.trim();
+					});
+
+					setSelectedTab(contextIdsWithNames[currentIndex - 1].name);
+				} else {
+					// 통과
+					setSelectedTab(tabs[1]);
+				}
+			}
+
+			/*
+				contextIdsWithNames 가 2개 이상이라면
+				selectedTab 이 이전으로 이동한다.
+				만약, 1개라면 myContext Tab 으로 이동한다.
+			*/
+		},
+		onError(error) {
+			console.error(error);
+		}
+	});
+
 	const tabClick = (name: string) => {
 		setSelectedTab(name);
-		if (name === 'Indicators') setIsAccordian(false);
-		if (name === 'MyContext') setIsAccordian(!isAccordian);
+		if (name === 'Indicators') setIsAccordianOpen(false);
+		if (name === 'MyContext') setIsAccordianOpen(!isAccordianOpen);
 	};
 
 	return (
@@ -45,15 +92,20 @@ export default function Menu({ selectedTab, setSelectedTab }: MenuProps) {
 				})}
 
 				<div className={clsx(styles.contexts)}>
-					{isAccordian &&
-						contextNamesWithKey?.map((context: ContextIdWithName, index: number) => {
+					{isAccordianOpen &&
+						contextIdsWithNames?.map((context: ContextIdWithName, index: number) => {
+							const { id: contextId, name } = context;
+
 							return (
-								<span
+								<div
 									key={index}
-									onClick={() => setSelectedTab(context.name)}
-									className={selectedTab === context.name ? clsx(styles.on) : ''}>
-									{context.name}
-								</span>
+									onClick={() => setSelectedTab(name)}
+									className={clsx(styles.context, { [styles.on]: selectedTab === name })}>
+									<span>{name}</span>
+									<span className={clsx(styles.deleteIcon)} onClick={() => deleteContextMutation.mutate(contextId)}>
+										<RiDeleteBin6Line />
+									</span>
+								</div>
 							);
 						})}
 				</div>
@@ -61,3 +113,15 @@ export default function Menu({ selectedTab, setSelectedTab }: MenuProps) {
 		</aside>
 	);
 }
+
+/*
+								<div key={index} className={clsx(styles.context)}>
+									<span
+										onClick={() => setSelectedTab(context.name)}
+										className={selectedTab === context.name ? clsx(styles.on) : ''}>
+										{context.name}
+										<RiDeleteBin6Line />
+									</span>
+								</div>
+
+*/
