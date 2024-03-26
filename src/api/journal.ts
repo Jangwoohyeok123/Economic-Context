da126f848f16f3922642bca07e6264ca3bf036d1
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-03-26 14:02:38 +0900

    feat: backend api 파일 분리

---
+++ src/api/journal.ts
@@ -0,0 +1,144 @@
+import { JournalData_Type, JournalParams_Type } from '@/types/backendType';
+import axios from 'axios';
+
+const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL_LOCAL;
+
+export const getContextJournals = async (contextId: number): Promise<JournalData_Type[]> => {
+	try {
+		if (typeof window === 'undefined') {
+			throw new Error('This function can only be used in the client-side');
+		}
+
+		const token = sessionStorage.getItem('token');
+		if (!token) {
+			throw new Error('No token found in sessionStorage');
+		}
+		const response = await axios.get(`${backendUrl}journal/${contextId}`, {
+			headers: {
+				Authorization: `Bearer ${token}`
+			}
+		});
+		return response.data;
+	} catch (error: any) {
+		if (error.response) {
+			console.error(`Error: ${error.response.status} - ${error.response.data}`);
+		} else if (error.request) {
+			console.error('Error: No response from server.');
+		} else {
+			console.error('Error: ', error.message);
+		}
+
+		throw new Error('Failed to fetch');
+	}
+};
+
+export const addJournal = async (userId: number, contextId: number, journalDataParams: JournalParams_Type) => {
+	try {
+		if (typeof window === 'undefined') {
+			throw new Error('This function can only be used in the client-side');
+		}
+
+		const token = sessionStorage.getItem('token');
+		if (!token) {
+			throw new Error('No token found in sessionStorage');
+		}
+		await axios.post(`${backendUrl}journal/${userId}/${contextId}`, journalDataParams, {
+			headers: {
+				Authorization: `Bearer ${token}`
+			}
+		});
+	} catch (error: any) {
+		if (error.response) {
+			console.error(`Error: ${error.response.status} - ${error.response.data}`);
+		} else if (error.request) {
+			console.error('Error: No response from server.');
+		} else {
+			console.error('Error: ', error.message);
+		}
+	}
+};
+
+export const deleteJournal = async (journalId: number) => {
+	try {
+		if (typeof window === 'undefined') {
+			throw new Error('This function can only be used in the client-side');
+		}
+
+		const token = sessionStorage.getItem('token');
+		if (!token) {
+			throw new Error('No token found in sessionStorage');
+		}
+		await axios.delete(`${backendUrl}journal/${journalId}`, {
+			headers: {
+				Authorization: `Bearer ${token}`
+			}
+		});
+	} catch (error: any) {
+		if (error.response) {
+			console.error(`Error: ${error.response.status} - ${error.response.data}`);
+		} else if (error.request) {
+			console.error('Error: No response from server.');
+		} else {
+			console.error('Error: ', error.message);
+		}
+	}
+};
+
+export const deleteJournals = async (contextId: number) => {
+	try {
+		if (typeof window === 'undefined') {
+			throw new Error('This function can only be used in the client-side');
+		}
+
+		const token = sessionStorage.getItem('token');
+		if (!token) {
+			throw new Error('No token found in sessionStorage');
+		}
+		await axios.delete(`${backendUrl}journal/${contextId}`, {
+			headers: {
+				Authorization: `Bearer ${token}`
+			}
+		});
+	} catch (error: any) {
+		if (error.response) {
+			console.error(`Error: ${error.response.status} - ${error.response.data}`);
+		} else if (error.request) {
+			console.error('Error: No response from server.');
+		} else {
+			console.error('Error: ', error.message);
+		}
+	}
+};
+
+export const modifyJournal = async (journalId: number, title: string, body: string) => {
+	try {
+		if (typeof window === 'undefined') {
+			throw new Error('This function can only be used in the client-side');
+		}
+
+		const token = sessionStorage.getItem('token');
+		if (!token) {
+			throw new Error('No token found in sessionStorage');
+		}
+		await axios.put(
+			`${backendUrl}journal/${journalId}`,
+			{
+				title: title,
+				body: body
+			},
+			{
+				headers: {
+					Authorization: `Bearer ${token}`
+				}
+			}
+		);
+	} catch (error: any) {
+		if (error.response) {
+			console.error(`Error: ${error.response.status} - ${error.response.data}`);
+		} else if (error.request) {
+			console.error('Error: No response from server.');
+		} else {
+			console.error('Error: ', error.message);
+		}
+	}
+};
