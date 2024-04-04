e415bc34687d2b77f44faf392b90cb9fea0fbe79
aajy <aajy.000@gmail.com>
2024-04-04 11:20:35 +0900

    test: 요청구문 로직 수정

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,12 +1,23 @@
 import { backendUrl } from '@/pages/_app';
 import { JwtAndGoogleUserData_Type } from '@/types/user';
 import axios from 'axios';
+const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || '';
+const redirectUrl =
+	process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URL || 'https://dev-economic-context.vercel.app/google-callback';
+const secret = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_PASSWORD || '';
 
 export async function getJwtAndGoogleUserData(authCode: string): Promise<JwtAndGoogleUserData_Type> {
+	const params = new URLSearchParams();
+	params.append('code', authCode);
+	params.append('client_id', clientId);
+	params.append('client_secret', secret);
+	params.append('redirect_uri', redirectUrl);
+	params.append('grant_type', 'authorization_code');
+
 	try {
-		const response = await axios.post(`https://oauth2.googleapis.com/token`, {
-			code: authCode
-		});
+		const response = await axios.post(`https://oauth2.googleapis.com/token`, params);
+		console.log('response: ', response);
+
 		const jwt = response.data[0];
 		const userData = response.data[1];
 
