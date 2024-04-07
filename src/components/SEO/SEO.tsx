5b77049349b452288970a957634e168d11c93f8d
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-04-07 11:58:00 +0900

    feat: search engine 최적화 작업
    - robots.txt 추가
    - SEO 컴포넌트 수정

--- src/components/SEO/SEO.tsx
+++ src/components/SEO/SEO.tsx
@@ -1,19 +1,22 @@
 import Head from 'next/head';
+
 interface SEO_Props {
 	title: string;
+	description: string;
 }
-export default function SEO({ title }: SEO_Props) {
+
+export default function SEO({
+	title,
+	description = '내가 원하는 경제지표를 편리하게 관리하고, 나의 일지를 작성할 수 있습니다. 로그인으로 시작하세요.'
+}: SEO_Props) {
 	return (
 		<Head>
-			<title>{`Economic Context | ${title}`}</title>
-			<meta
-				name='description'
-				content='내가 원하는 경제지표를 편리하게 관리하고, 나의 일지를 작성할 수 있습니다. 로그인으로 시작하세요.'
-			/>
+			<title>{`Economic Context | ${title}page`}</title>
+			<meta name='description' content={description} />
 			<meta charSet='UTF-8' />
-			<meta property='og:title' content={`Economic Context : 경제지표를 더 쉽게 관리하자!`} />
+			<meta property='og:title' content='Economic Context : 경제지표를 더 쉽게 관리하자!' />
 			<meta property='og:type' content='website' />
-			<meta property='og:url' content='https://localhost:3000' />
+			<meta property='og:url' content={process.env.NEXT_PUBLIC_FRONT_URL} />
 			<meta name='keywords' content='economic, EconomicContext, indicator'></meta>
 		</Head>
 	);
