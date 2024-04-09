53f42a82783368929d53fdd3db674b9181d6761b
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-04-09 17:33:28 +0900

    refactor: const x 관련된 불필요한 type 제거

--- src/components/charts/line/LineChart.tsx
+++ src/components/charts/line/LineChart.tsx
@@ -10,7 +10,7 @@ export interface LineChart_Props {
 	children?: React.ReactElement;
 	height?: number;
 	width?: number;
-	values: DateAndValue_Type[] | [undefined, undefined];
+	values: DateAndValue_Type[];
 	className?: string;
 	seriesId?: string;
 }
@@ -47,64 +47,63 @@ const LineChart = ({ indicator, values, width, height, className, seriesId }: Li
 
 	// chart 를 세팅하는 라이브러리 로직입니다.
 	useEffect(() => {
-		const width = svgContainerRef.current?.offsetWidth || 0;
-		const height = svgContainerRef.current?.offsetHeight || 0;
-		const marginTop = 20;
-		const marginRight = 40;
-		const marginBottom = 40;
-		const marginLeft = 40;
-		const x = d3.scaleUtc(
-			d3.extent(values as DateAndValue_Type[], (value: DateAndValue_Type) => value.date) as unknown as [Date, Date],
-			[0, width]
-		);
-		const maxValue = d3.max(values as DateAndValue_Type[], (value: DateAndValue_Type) => Number(value.value));
-		const y = d3.scaleLinear([0, Number(maxValue) * 1.3], [height - marginBottom, marginTop]);
+		if (values) {
+			const width = svgContainerRef.current?.offsetWidth || 0;
+			const height = svgContainerRef.current?.offsetHeight || 0;
+			const marginTop = 20;
+			const marginRight = 40;
+			const marginBottom = 40;
+			const marginLeft = 40;
+			const x = d3.scaleUtc(d3.extent(values, (value: DateAndValue_Type) => value.date) as [Date, Date], [0, width]);
+			const maxValue = d3.max(values, (value: DateAndValue_Type) => Number(value.value));
+			const y = d3.scaleLinear([0, Number(maxValue) * 1.3], [height - marginBottom, marginTop]);
 
-		const line = d3
-			.line<DateAndValue_Type>()
-			.x(d => x(d.date))
-			.y(d => y(Number(d.value)));
+			const line = d3
+				.line<DateAndValue_Type>()
+				.x(d => x(d.date))
+				.y(d => y(Number(d.value)));
 
-		const svg = d3
-			.select(svgRef.current)
-			.attr('width', width)
-			.attr('height', height)
-			.attr('viewBox', [0, 0, width, height])
-			.attr('style', 'max-width: 100%; height: 100%');
+			const svg = d3
+				.select(svgRef.current)
+				.attr('width', width)
+				.attr('height', height)
+				.attr('viewBox', [0, 0, width, height])
+				.attr('style', 'max-width: 100%; height: 100%');
 
-		// 중첩되는 chart 제거
-		svg.selectAll('*').remove();
+			// 중첩되는 chart 제거
+			svg.selectAll('*').remove();
 
-		// axis bottom 추가
-		svg
-			.append('g')
-			.attr('transform', `translate(0,${height - marginBottom})`)
-			.call(
-				d3
-					.axisBottom(x)
-					.ticks(width / 200)
-					.tickSizeOuter(10)
-			);
+			// axis bottom 추가
+			svg
+				.append('g')
+				.attr('transform', `translate(0,${height - marginBottom})`)
+				.call(
+					d3
+						.axisBottom(x)
+						.ticks(width / 200)
+						.tickSizeOuter(10)
+				);
 
-		svg
-			.append('g')
-			.attr('transform', `translate(${marginLeft},0)`)
-			.call(d3.axisLeft(y).ticks(height / 40))
-			.call(g => g.select('.domain').remove())
-			.call(g =>
-				g
-					.selectAll('.tick line')
-					.clone()
-					.attr('x2', width - marginLeft - marginRight)
-					.attr('stroke-opacity', 0.1)
-			);
+			svg
+				.append('g')
+				.attr('transform', `translate(${marginLeft},0)`)
+				.call(d3.axisLeft(y).ticks(height / 40))
+				.call(g => g.select('.domain').remove())
+				.call(g =>
+					g
+						.selectAll('.tick line')
+						.clone()
+						.attr('x2', width - marginLeft - marginRight)
+						.attr('stroke-opacity', 0.1)
+				);
 
-		svg
-			.append('path')
-			.attr('fill', 'none')
-			.attr('stroke', 'steelblue')
-			.attr('stroke-width', 1.5)
-			.attr('d', line(values as DateAndValue_Type[]));
+			svg
+				.append('path')
+				.attr('fill', 'none')
+				.attr('stroke', 'steelblue')
+				.attr('stroke-width', 1.5)
+				.attr('d', line(values as DateAndValue_Type[]));
+		}
 	}, [values]);
 
 	return (
