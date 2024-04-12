2a6c8de158390cfca555a86d0dc451abb18f9318
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-04-12 15:23:36 +0900

    feat: transform: scale 제거
    - range 를 통해서 크기제어하기

--- src/components/charts/line/LineChart.tsx
+++ src/components/charts/line/LineChart.tsx
@@ -94,11 +94,14 @@ const LineChart = ({ indicator, duration, values, width = 100, height = 65, clas
 			const xAxisSize = 30;
 
 			// const yDomain = d3.extent(slicedValues, (value: DateAndValue_Type) => Number(value.value));
-			const [xDomain, xRange, yDomain, yRange] = [
+			const [xDomain, xRange] = [
 				d3.extent(slicedValues, (value: DateAndValue_Type) => value.date) as [Date, Date],
-				[0, svgWidth],
+				[10, svgWidth - 100]
+			];
+
+			const [yDomain, yRange] = [
 				d3.extent(slicedValues, (value: DateAndValue_Type) => Number(value.value)) as [number, number],
-				[0, svgHeight - 100] // y 축 길이 제어는 range 로 해라
+				[40, svgHeight - 50]
 			];
 
 			const [utcScale, linearScale] = [d3.scaleUtc(xDomain, xRange), d3.scaleLinear(yDomain, yRange)];
@@ -116,13 +119,13 @@ const LineChart = ({ indicator, duration, values, width = 100, height = 65, clas
 			// each 는 x 축에 벗어나는 부분을 제어한다. offset 이 커질수록 축의 가장자리에 숫자가 나오지 않는다.
 			svg
 				.append('g')
-				.attr('style', `transform: translate(0, calc(100% - ${xAxisSize}px)) scale(0.92);`)
-				.call(d3.axisBottom(utcScale).ticks(10).tickSizeOuter(0))
+				.attr('style', `transform: translate(0, calc(100% - ${xAxisSize}px));`)
+				.call(d3.axisBottom(utcScale).ticks(5).tickSizeOuter(0))
 				.selectAll('.tick')
 				.each(function (date, index, nodes) {
 					const offset = 100;
 					const node: Element = nodes[index];
-					if (index === 0 && node.getBoundingClientRect().x - xAxisStartPosition < offset * 0.7) {
+					if (index === 0 && node.getBoundingClientRect().x - xAxisStartPosition < offset * 0.3) {
 						d3.select(this).remove();
 					}
 					if (index === nodes.length - 1 && xAxisLastPosition - node.getBoundingClientRect().x < offset) {
@@ -131,25 +134,25 @@ const LineChart = ({ indicator, duration, values, width = 100, height = 65, clas
 				});
 
 			// axisRight y 축
-			// svg
-			// 	.append('g')
-			// 	.attr('transform', `translate(${svgWidth}, 0) scale(0.9)`) // y 축을 오른쪽으로 이동
-			// 	.call(d3.axisRight(linearScale).ticks(10)) // 오른쪽 축 함수 사용
-			// 	.call(g => g.select('.domain').remove()) // 축 라인 제거
-			// 	.call(g =>
-			// 		g
-			// 			.selectAll('.tick line')
-			// 			.clone() // 틱 라인 확장
-			// 			.attr('x2', '-100%') // y 축 우측으로 이동시키기
-			// 			.attr('stroke-opacity', 0.15)
-			// 	);
+			svg
+				.append('g')
+				.attr('style', `transform: translate(calc(100% - ${50}px), ${-xAxisSize + 7}px); `)
+				.call(d3.axisRight(linearScale).ticks(5))
+				.call(g => g.select('.domain').remove())
+				.call(g =>
+					g
+						.selectAll('.tick line')
+						.clone() // 틱 라인 확장
+						.attr('x2', '-100%') // y 축 우측으로 이동시키기
+						.attr('stroke-opacity', 0.15)
+				);
 
-			// svg
-			// 	.append('path')
-			// 	.attr('fill', 'none')
-			// 	.attr('stroke', 'steelblue')
-			// 	.attr('stroke-width', 1.5)
-			// 	.attr('d', line(slicedValues as DateAndValue_Type[]));
+			svg
+				.append('path')
+				.attr('fill', 'none')
+				.attr('stroke', 'steelblue')
+				.attr('stroke-width', 1.5)
+				.attr('d', line(slicedValues as DateAndValue_Type[]));
 		}
 	}, [values]);
 
