90c61e68e868cbcc528abca01bd83e906b6d7b3c
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-04-14 15:29:55 +0900

    style: chartDescription 부분 스타일링

--- src/components/chartDescription/ChartDescription.tsx
+++ src/components/chartDescription/ChartDescription.tsx
@@ -1,13 +1,26 @@
 import { Indicator_Type } from '@/types/fred';
 import styled from 'styled-components';
 
-const DescriptionWrapper = styled.div``;
+const DescriptionWrapper = styled.div`
+	padding-top: 30px;
+
+	h3 {
+		/* border-top: 1px solid #ccc; */
+		border-bottom: 1px solid #ccc;
+		height: 50px;
+		padding: 0 20px;
+		font-size: 1.5rem;
+		font-weight: 500;
+		display: flex;
+		justify-content: left;
+		align-items: center;
+	}
+`;
 
 const Section = styled.p`
-	padding-bottom: 15px;
-	font-size: 0.85rem;
-	opacity: 0.9;
-	width: 60%;
+	opacity: 0.85;
+	width: 100%;
+	padding: 20px;
 `;
 
 interface ChartDescription_Props {
@@ -21,6 +34,7 @@ export default function ChartDescription({ indicator, children }: ChartDescripti
 	const sections = notes?.split('\n');
 	return (
 		<DescriptionWrapper>
+			<h3>notes</h3>
 			{sections?.map((section, index) => {
 				if (section.length > 1) {
 					return <Section key={index}>{section}</Section>;
