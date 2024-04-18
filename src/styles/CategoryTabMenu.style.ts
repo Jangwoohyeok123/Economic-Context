33f12ff482b9b0d3ba94b43581cf45e7e9247df7
aajy <aajy.000@gmail.com>
2024-04-18 15:51:43 +0900

    feat: category menu 스타일링
    - category마다 정해진 color를 적용한 스타일링 추가
    
    Related to:#155

--- src/styles/CategoryTabMenu.style.ts
+++ src/styles/CategoryTabMenu.style.ts
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
-
-export const TabMenu = styled.div`
+interface MenuButton_Props {
+	$categoryColor: string;
+}
+export const TabMenuWrap = styled.div`
 	width: 100%;
 	padding-top: 50px;
 	text-align: center;
@@ -10,19 +12,19 @@ export const TabMenu = styled.div`
 	gap: 20px;
 
 	button {
-		border: 1px solid #ccc;
-		padding: 10px 20px;
-		cursor: pointer;
+		padding: 14px 30px;
 		transition: 0.3s;
-
-		&:hover {
-			color: #fff;
-			background: #111;
-		}
-
-		&.on {
-			color: #fff;
-			background: #111;
+		border-radius: 3px;
+		cursor: pointer;
+		font-size: 0.8rem;
+		display: flex;
+		justify-content: space-between;
+		position: relative;
+		overflow: hidden;
+		> span.icon {
+			position: absolute;
+			right: -5px;
+			bottom: -5px;
 		}
 	}
 	@media screen and (max-width: 1024px) {
@@ -33,5 +35,18 @@ export const TabMenu = styled.div`
 				width: 40%;
 			}
 		}
+	}
+`;
+export const MenuButton = styled.button<MenuButton_Props>`
+	border: 1px solid ${props => props.$categoryColor};
+	color: ${props => props.$categoryColor};
+	&:hover {
+		color: #fff;
+		background: ${props => props.$categoryColor};
+	}
+
+	&.on {
+		color: #fff;
+		background: ${props => props.$categoryColor};
 	}
 `;
