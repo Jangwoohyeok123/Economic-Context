60e482b8ad463e27cf5ef44ec0f807ca066c9341
aajy <aajy.000@gmail.com>
2024-04-18 21:28:40 +0900

    feat:category별로 icon적용하기
    - svg를 컴포넌트로 쓸 수 있는 웹팩설치 및 설정
    - svg파일에서 변경을 원하는 곳에 current를 작성하고, 컴포넌트에서 값을
      작성해 변경 할 수 있음
    
    Resolves: #155

--- src/components/categoryTabMenu/CategoryTabMenu.tsx
+++ src/components/categoryTabMenu/CategoryTabMenu.tsx
@@ -1,15 +1,14 @@
 import clsx from 'clsx';
 import * as S from '@/styles/CategoryTabMenu.style';
 import const_categoryColor from '@/const/categoryColor';
-import interest_mortgage from '@/public/interest_mortgage.svg';
-import interest_fed from '@/public/interest_fed.svg';
-import materials from '@/public/materials.svg';
-import gdp from '@/public/gdp.svg';
-import exchange from '@/public/exchange.svg';
-import production from '@/public/production.svg';
-import consume from '@/public/consume.svg';
-import labor from '@/public/labor.svg';
-import Image from 'next/image';
+import Interest_mortgage from '@/public/interest_mortgage.svg';
+import Interest_fed from '@/public/interest_fed.svg';
+import Materials from '@/public/materials.svg';
+import Gdp from '@/public/gdp.svg';
+import Exchange from '@/public/exchange.svg';
+import Production from '@/public/production.svg';
+import Consume from '@/public/consume.svg';
+import Labor from '@/public/labor.svg';
 
 interface CategoryTabMenu_Props {
 	categoryNames: string[];
@@ -18,7 +17,7 @@ interface CategoryTabMenu_Props {
 }
 
 /** categoryNames 배열을 전달하면 tab 기능을 제공한다. */
-export default function CategoryTabMenu({ categoryNames, categoryIndex, selectCategory }: CategoryTabMenu_Props) {
+export default function CategoryTabMenu({ categoryNames, categoryIndex: selectedCategory, selectCategory }: CategoryTabMenu_Props) {
 	return (
 		<S.TabMenuWrap>
 			{categoryNames.map((_, idx) => {
@@ -29,10 +28,29 @@ export default function CategoryTabMenu({ categoryNames, categoryIndex, selectCa
 							selectCategory(e, idx);
 						}}
 						$categoryColor={const_categoryColor[idx].color}
-						className={clsx({ on: idx === categoryIndex })}>
+						className={clsx({ on: idx === selectedCategory })}>
 						{categoryNames[idx]}
 						<span className={clsx('icon')}>
-							<Image src={interest_mortgage} alt='icon' width={30} height={30} />
+							{const_categoryColor[idx].name === 'interest_mortgage' && (
+								<Interest_mortgage fill={idx === selectedCategory ? '#fefefe' : const_categoryColor[idx].color} />
+							)}
+							{const_categoryColor[idx].name === 'interest_fed' && (
+								<Interest_fed stroke={idx === selectedCategory ? '#fefefe' : const_categoryColor[idx].color} />
+							)}
+							{const_categoryColor[idx].name === 'materials' && (
+								<Materials stroke={idx === selectedCategory ? '#fefefe' : const_categoryColor[idx].color} />
+							)}
+							{const_categoryColor[idx].name === 'gdp' && <Gdp fill={idx === selectedCategory ? '#fefefe' : const_categoryColor[idx].color} />}
+							{const_categoryColor[idx].name === 'exchange' && (
+								<Exchange stroke={idx === selectedCategory ? '#fefefe' : const_categoryColor[idx].color} />
+							)}
+							{const_categoryColor[idx].name === 'production' && (
+								<Production fill={idx === selectedCategory ? '#fefefe' : const_categoryColor[idx].color} />
+							)}
+							{const_categoryColor[idx].name === 'consume' && (
+								<Consume fill={idx === selectedCategory ? '#fefefe' : const_categoryColor[idx].color} />
+							)}
+							{const_categoryColor[idx].name === 'labor' && <Labor stroke={idx === selectedCategory ? '#fefefe' : const_categoryColor[idx].color} />}
 						</span>
 					</S.MenuButton>
 				);
