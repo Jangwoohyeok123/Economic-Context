a52deb41d35fc646209b22942bd7741c55dcc71b
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-04-19 10:44:03 +0900

    refactor: changeNameToCategoryId 변경

--- src/utils/changeNameToCategoryId.ts
+++ src/utils/changeNameToCategoryId.ts
@@ -1,3 +1,4 @@
+import const_categoryId from '@/const/categoryId';
 import const_categoryTypes from '@/const/categoryId';
 
 export const changeNameToCategoryId = (categoryName: string): number => {
@@ -10,12 +11,26 @@ export const changeNameToCategoryId = (categoryName: string): number => {
 	return type;
 };
 
-export const changeCategoryIdToName = (categoryType: number): string => {
+export const changeCategoryIdToName = (categoryId: number): string => {
 	let categoryName = 'Interest';
-	if (categoryType === const_categoryTypes.interest_mortgage) return (categoryName = 'Interest');
-	if (categoryType === const_categoryTypes.exchange) return (categoryName = 'Exchange');
-	if (categoryType === const_categoryTypes.production) return (categoryName = 'Production');
-	if (categoryType === const_categoryTypes.consume) return (categoryName = 'Consume');
+	if (categoryId === const_categoryTypes.interest_mortgage) return (categoryName = 'Interest');
+	if (categoryId === const_categoryTypes.exchange) return (categoryName = 'Exchange');
+	if (categoryId === const_categoryTypes.production) return (categoryName = 'Production');
+	if (categoryId === const_categoryTypes.consume) return (categoryName = 'Consume');
 
 	return categoryName;
+};
+
+export const changeCategoryIdToColor = (categoryId: number): string => {
+	let categoryColor = '';
+	if (categoryId === const_categoryId.interest_mortgage) categoryColor = '#E5B04F';
+	if (categoryId === const_categoryId.interest_fed) categoryColor = '#B88855';
+	if (categoryId === const_categoryId.materials) categoryColor = '#D8C3A0';
+	if (categoryId === const_categoryId.gdp) categoryColor = '#A9C9C2';
+	if (categoryId === const_categoryId.exchange) categoryColor = '#59AEC3';
+	if (categoryId === const_categoryId.production) categoryColor = '#FEBCD7';
+	if (categoryId === const_categoryId.consume) categoryColor = '#CBEDFE';
+	if (categoryId === const_categoryId.labor) categoryColor = '#7CB8EF';
+
+	return categoryColor;
 };
