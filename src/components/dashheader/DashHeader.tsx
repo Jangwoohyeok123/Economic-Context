9960b5c7e30bf4264503608cd230807f3f51fb16
aajy <aajy.000@gmail.com>
2024-04-19 20:26:31 +0900

    feat: 프로필 영역 작업 - DashHeader 프로필 DefaultProfile추가
    - ProfileImage styled-component 추가
    - defaultProfile 이미지 추가
    - DashHeader  defaultProfile이미지 추가
    
    Related  to: ##174

--- src/components/dashheader/DashHeader.tsx
+++ src/components/dashheader/DashHeader.tsx
@@ -2,26 +2,34 @@ import clsx from 'clsx';
 import styles from './DashHeader.module.scss';
 import { Store_Type } from '@/types/redux';
 import { useSelector } from 'react-redux';
-import Image from 'next/image';
-
+import defaultProfile from '@/public/defaultProfile.png';
+import * as S from '@/styles/ProfileImage.style';
+import { useState } from 'react';
 interface Dashboard_Prop {
 	selectedTab: string;
 }
 
 export default function DashHeader({ selectedTab }: Dashboard_Prop) {
 	const user = useSelector((state: Store_Type) => state.user);
+	const [loaded, setLoaded] = useState(true);
 	const profileImageUrl = user.picture_url;
+
 	return (
 		<header className={clsx(styles.DashHeader)}>
 			<h2>{selectedTab}</h2>
-			<Image
-				className={clsx(styles.userImage)}
-				src={profileImageUrl}
-				alt='User profile image'
-				width={50} // 이 값은 적절하게 조정하세요
-				height={50} // 이 값도 적절하게 조정하세요
-				unoptimized={true} // 필요에 따라 최적화를 비활성화할 수 있습니다
-			/>
+			{profileImageUrl && loaded ? (
+				<S.ProfileImage
+					src={profileImageUrl}
+					alt='User profile image'
+					width={40} // 이 값은 적절하게 조정하세요
+					height={40} // 이 값도 적절하게 조정하세요
+					onLoad={() => setLoaded(true)}
+					onError={() => setLoaded(false)}
+					unoptimized={true} // 필요에 따라 최적화를 비활성화할 수 있습니다
+				/>
+			) : (
+				<S.ProfileImage src={defaultProfile} alt='User default profile image' width={40} height={40} unoptimized={true} />
+			)}
 		</header>
 	);
 }
