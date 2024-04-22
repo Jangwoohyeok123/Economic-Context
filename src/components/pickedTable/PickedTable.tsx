c1682cbb6fe59a417c64d6a8c6688b8f5fd65e5b
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-04-22 19:09:57 +0900

    refactor: 부모요소로부터 checkedFavorite_List를 전달받는 구조로 변경
    - 형제컴포넌트에서도 checkedFavorite_List를 사용중이기 때문에 부모요소로부터 받아오는 구조로 변경

--- src/components/pickedTable/PickedTable.tsx
+++ src/components/pickedTable/PickedTable.tsx
@@ -1,9 +1,10 @@
+import { FavoriteIndicator_Type } from '@/types/favorite';
 import styled from 'styled-components';
 
 const PickedTableContainer = styled.div`
 	height: 55%;
 	width: 100%;
-	margin-bottom: 10px;
+	margin-bottom: 20px;
 
 	div {
 		height: 35px;
@@ -33,24 +34,6 @@ const PickedTableContainer = styled.div`
 		}
 	}
 
-	/* 
-		transition: 0.5s;
-
-		&::-webkit-scrollbar {
-			width: 3px;
-		}
-
-		&::-webkit-scrollbar-thumb {
-			width: 3px;
-			background: #ccc;
-			visibility: hidden;
-		}
-
-		&:hover::-webkit-scrollbar-thumb {
-			visibility: visible;
-		}
-	*/
-
 	.tableBody {
 		overflow-y: scroll;
 		height: calc(100% - 35px);
@@ -72,24 +55,43 @@ const PickedTableContainer = styled.div`
 		.row {
 			display: flex;
 			align-items: center;
-			span {
+
+			div {
+				display: flex;
+				justify-content: center;
+				align-items: center;
 				width: 60%;
-				display: inline-block;
-				text-align: center;
 			}
 
-			span:nth-of-type(2) {
+			div:nth-of-type(1) {
+				gap: 10px;
+				padding-left: 3px;
+				justify-content: left;
+				overflow: hidden; /* 내용이 넘치면 잘라냄 */
+
+				input[type='checkbox'] {
+					transform: scale(1.2);
+				}
+			}
+
+			div:nth-of-type(2) {
 				width: 20%;
 			}
 
-			span:nth-of-type(3) {
+			div:nth-of-type(3) {
 				width: 20%;
 			}
 		}
 	}
 `;
 
-export default function PickedTable() {
+interface PickedTable_Props {
+	checkedFavorite_List: FavoriteIndicator_Type[];
+	isChecked_List: boolean[];
+	setIsChecked_List: React.Dispatch<React.SetStateAction<boolean[]>>;
+}
+
+export default function PickedTable({ checkedFavorite_List, isChecked_List, setIsChecked_List }: PickedTable_Props) {
 	return (
 		<PickedTableContainer>
 			<div className='tableHead'>
@@ -98,86 +100,27 @@ export default function PickedTable() {
 				<span>category</span>
 			</div>
 			<div className='tableBody'>
-				<div className='row'>
-					<span>Indicator</span>
-					<span>volality</span>
-					<span>category</span>
-				</div>
-				<div className='row'>
-					<span>Indicator</span>
-					<span>volality</span>
-					<span>category</span>
-				</div>
-				<div className='row'>
-					<span>Indicator</span>
-					<span>volality</span>
-					<span>category</span>
-				</div>
-				<div className='row'>
-					<span>Indicator</span>
-					<span>volality</span>
-					<span>category</span>
-				</div>{' '}
-				<div className='row'>
-					<span>Indicator</span>
-					<span>volality</span>
-					<span>category</span>
-				</div>{' '}
-				<div className='row'>
-					<span>Indicator</span>
-					<span>volality</span>
-					<span>category</span>
-				</div>{' '}
-				<div className='row'>
-					<span>Indicator</span>
-					<span>volality</span>
-					<span>category</span>
-				</div>{' '}
-				<div className='row'>
-					<span>Indicator</span>
-					<span>volality</span>
-					<span>category</span>
-				</div>{' '}
-				<div className='row'>
-					<span>Indicator</span>
-					<span>volality</span>
-					<span>category</span>
-				</div>{' '}
-				<div className='row'>
-					<span>Indicator</span>
-					<span>volality</span>
-					<span>category</span>
-				</div>{' '}
-				<div className='row'>
-					<span>Indicator</span>
-					<span>volality</span>
-					<span>category</span>
-				</div>{' '}
-				<div className='row'>
-					<span>Indicator</span>
-					<span>volality</span>
-					<span>category</span>
-				</div>{' '}
-				<div className='row'>
-					<span>Indicator</span>
-					<span>volality</span>
-					<span>category</span>
-				</div>{' '}
-				<div className='row'>
-					<span>Indicator</span>
-					<span>volality</span>
-					<span>category</span>
-				</div>{' '}
-				<div className='row'>
-					<span>Indicator</span>
-					<span>volality</span>
-					<span>category</span>
-				</div>{' '}
-				<div className='row'>
-					<span>Indicator</span>
-					<span>volality</span>
-					<span>category</span>
-				</div>
+				{checkedFavorite_List.map((favoriteIndicator, index) => {
+					const { title } = favoriteIndicator;
+
+					return (
+						<div className='row' key={index}>
+							<div
+								onClick={() => {
+									const newCheckedList = [...isChecked_List];
+									// 배열의 해당 요소를 토글
+									newCheckedList[index] = !newCheckedList[index];
+									// 새로운 배열로 상태 업데이트
+									setIsChecked_List(newCheckedList);
+								}}>
+								<input type='checkbox' checked={isChecked_List[index]}></input>
+								<span>{title}</span>
+							</div>
+							<div>volality</div>
+							<div>category</div>
+						</div>
+					);
+				})}
 			</div>
 		</PickedTableContainer>
 	);
