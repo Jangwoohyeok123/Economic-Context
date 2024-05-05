0e6b710cdc5f9fb7492dec06a4d181c3d1d4107f
aajy <aajy.000@gmail.com>
2024-05-06 01:48:41 +0900

    feat: JournalForm 등록 API연동
    - ref를 사용하고자 분리했던 QuillEditor의 데이터를 상태값으로 사용하여
      다시 합침
    - TODO :: 구체적인 validation조건 상의하기
    
    Related to: #156

--- src/components/journalsSection/journalForm/JournalForm.tsx
+++ src/components/journalsSection/journalForm/JournalForm.tsx
@@ -3,15 +3,24 @@ import const_queryKey from '@/const/queryKey';
 import { JournalParams_Type } from '@/types/journal';
 import { Store_Type } from '@/types/redux';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { useEffect, useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { BsChevronDown, BsChevronUp } from 'react-icons/bs';
 import { FcIdea, FcApproval, FcCloseUpMode, FcClock } from 'react-icons/fc';
 import { DropDownMenu, Dropdown, Form, JournalFormWrap } from '@/styles/Journal.style';
-import 'react-quill/dist/quill.snow.css'; //
+import 'react-quill/dist/quill.snow.css';
 import ProfileImage from '@/components/common/profileImage/ProfileImage';
-import QuillEditor from './QuillEditor';
-
+import dynamic from 'next/dynamic';
+const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
+const modules = {
+	toolbar: [
+		['bold', 'italic', 'underline', 'strike'], // 기본 툴바 옵션
+		['clean'], // 편집 제거
+		[{ list: 'ordered' }, { list: 'bullet' }], // 목록 옵션
+		['link'], // 링크와 이미지 옵션
+		['blockquote', 'code-block'] // 인용구와 코드 블록 옵션
+	]
+};
 interface JournalForm_Props {
 	contextId: number;
 }
@@ -23,7 +32,8 @@ export default function JournalForm({ contextId }: JournalForm_Props) {
 	const queryClient = useQueryClient();
 	const iconList = ['idea', 'approval', 'flower', 'clock'];
 	const titleInputRef = useRef<HTMLInputElement>(null);
-	const [body, setBody] = useState('');
+	const [quillValue, setQuillValue] = useState('');
+	const quillRef = useRef<typeof ReactQuill>(null);
 	const addJournalMutation = useMutation({
 		mutationFn: ({ userId, contextId, journalDataParams }: { userId: number; contextId: number; journalDataParams: JournalParams_Type }) =>
 			addJournal(userId, contextId, journalDataParams),
@@ -38,24 +48,23 @@ export default function JournalForm({ contextId }: JournalForm_Props) {
 	});
 	const requestAddJournal = (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
-
-		if (journalDataParams.title.length > 0 && journalDataParams.body.length > 0) {
-			addJournalMutation.mutate({ userId, contextId, journalDataParams });
+		if (journalDataParams.title.length <= 0) {
+			return alert('제목을 작성해주세요.');
+		}
+		if (!quillValue || quillValue === '<p><br></p>') {
+			//TODO:: 전체지워도 남아있는 구문.
+			setQuillValue('');
+			return alert('본문을 작성해주세요.');
 		} else {
-			if (!journalDataParams.title.length) {
-				return alert('제목을 작성해주세요.');
-			}
-			if (!body.length) {
-				return alert('본문을 작성해주세요.');
-			} else {
-				setJournalDataParams(prevParams => {
-					let newParams = { ...prevParams };
-					newParams = {
-						...newParams,
-						body: body
-					};
-					return newParams;
-				});
+			let newParams = { ...journalDataParams };
+			newParams = {
+				...newParams,
+				body: quillValue
+			};
+			setJournalDataParams(newParams);
+
+			if (journalDataParams.title.length > 0 && newParams.body.length > 0) {
+				return addJournalMutation.mutate({ userId, contextId, journalDataParams: newParams });
 			}
 		}
 	};
@@ -145,7 +154,16 @@ export default function JournalForm({ contextId }: JournalForm_Props) {
 						<label htmlFor='title'>Add a title</label>
 						<input ref={titleInputRef} type='text' name='title' placeholder='Write a journal title' onChange={() => onChangeJournalData('title')} />
 						<label htmlFor='title'>Add a body</label>
-						<QuillEditor setBody={setBody} />
+						<ReactQuill
+							theme={'snow'}
+							modules={modules}
+							value={quillValue || ''}
+							onChange={setQuillValue}
+							placeholder='Write a journal entry'
+							style={{
+								width: '100%'
+							}}
+						/>
 					</div>
 					<div className='formButton'>
 						<button>등록</button>
