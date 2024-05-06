a2b4b8f7d4aa42be8fed3c1a0f1c2fee3848afeb
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-05-06 22:36:48 +0900

    style: mainImage 추가하기

--- src/components/mainImage/MainImage.tsx
+++ src/components/mainImage/MainImage.tsx
@@ -8,7 +8,26 @@ export default function MainImage() {
 		<MainImageContainer className={clsx(poppins.variable, roboto.variable)}>
 			<Image alt='mainpage_Image' src='/mainImage.jpg' fill />
 			<article>
-				<h2>Create your economic context</h2>
+				<h2>Trusted economic indicator</h2>
+				<p>make your economic context</p>
+				<div className='top'>
+					<div className='item'>
+						<span>8</span>
+						<div>Category</div>
+					</div>
+					<div className='lineWrapper item'>
+						<div className='line'></div>
+					</div>
+					<div className='item'>
+						<span>20</span>
+						<div>Indicators</div>
+					</div>
+				</div>
+
+				<div className='buttons'>
+					<button>About</button>
+					<button>Login</button>
+				</div>
 			</article>
 		</MainImageContainer>
 	);
@@ -16,29 +35,86 @@ export default function MainImage() {
 
 const MainImageContainer = styled.div`
 	font-family: var(--baseFont);
-	height: calc(var(--headerSize) + 40vh);
-	padding-top: var(--headerSize);
-	background: #333;
+	height: calc(var(--headerSize) + 55vh);
 	width: 100%;
 	position: relative;
+	display: flex;
+	padding-top: var(--headerSize);
+	justify-content: center;
+	align-items: center;
 
 	img {
-		top: 60px;
-		left: 0px;
+		width: 100%;
 		height: 100%;
 	}
 
-	> article {
+	article {
 		position: relative;
-		width: 80%;
-		margin: 0 auto;
+		color: #fff;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 10px;
 
 		h2 {
-			padding-top: 100px;
-			width: 450px;
-			color: #fff;
-			font-size: 3rem;
-			font-weight: 400;
+			font-size: 40px;
+			font-weight: 500;
+		}
+
+		p {
+			opacity: 0.7;
+			font-size: 1.3rem;
+		}
+
+		.top {
+			display: flex;
+			width: 250px;
+
+			.item {
+				flex: 1;
+				width: 33%;
+				position: relative;
+				display: flex;
+				flex-direction: column;
+				justify-content: center;
+				align-items: center;
+
+				> span {
+					font-size: 3.5rem;
+				}
+
+				.line {
+					margin: 0 auto;
+
+					width: 1px;
+					height: 50px;
+					background: #aaa;
+					display: flex;
+					justify-content: center;
+				}
+			}
+		}
+
+		.buttons {
+			padding-top: 15px;
+			display: flex;
+			gap: 30px;
+
+			button {
+				width: 150px;
+				height: 40px;
+				border: none;
+				color: #fff;
+				cursor: pointer;
+			}
+
+			button:nth-of-type(1) {
+				background: #ab958b;
+			}
+
+			button:nth-of-type(2) {
+				background: #5f5755;
+			}
 		}
 	}
 `;
