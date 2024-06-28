3184f62e8e7a608bb21e23fcfa205f082fee60df
aajy <aajy.000@gmail.com>
2024-06-28 23:50:59 +0900

    docs: IndicatorsTab 파일 styledcomponent분리
    - 파일 분리 후 styledComponent import

--- src/components/indicatorsTab/IndicatorsTab.tsx
+++ src/components/indicatorsTab/IndicatorsTab.tsx
@@ -1,185 +1,14 @@
-import clsx from 'clsx';
-import styles from './IndicatorsTab.module.scss';
 import { useRef, useState } from 'react';
 import useFavoriteQuery from '@/hooks/useFavoriteQuery';
-import useFavoriteMutation from '@/hooks/useFavoriteMutation';
-import { changeCategoryIdToName } from '@/utils/changeNameToCategoryId';
-import LoginAlertModal from '../modals/loginAlertModal/LoginAlertModal';
+import { FavoriteContainer, LeftContainer, RightContainer } from '@/styles/IndicatorsTab.style';
 import { FavoriteIndicator_Type } from '@/types/favorite';
-import styled from 'styled-components';
 import const_categoryId, { categoryIds } from '@/const/categoryId';
-import CreateContextSection from '../createContextSection/CreateContextSection';
-import { MdExpandMore } from 'react-icons/md';
 import Accordian from '../accordian/Accordian';
 import CategoryTabMenu from '../categoryTabMenu/CategoryTabMenu';
-
-const FavoriteContainer = styled.section`
-	display: flex;
-	width: 100%;
-	height: calc(100vh - var(--headerSize));
-	padding: 20px 40px;
-	justify-content: space-between;
-`;
-
-const LeftContainer = styled.div`
-	width: 50%;
-	overflow: hidden;
-
-	// Tabmenu는 고정px
-	.categoryTabMenuWrapper {
-		width: 550px;
-		min-height: 140px;
-		margin: 0 auto;
-		display: flex;
-		align-items: center;
-	}
-
-	// favoriteList는 나머지 여백
-	.favoriteList {
-		height: calc(100% - 180px);
-		overflow-y: auto;
-		transition: 0.5s;
-
-		> .item {
-			padding: 0 20px;
-			width: 100%;
-			height: 50px;
-			display: flex;
-			align-items: center;
-			border-top: 1px solid #ccc;
-			gap: 20px;
-
-			input[type='checkbox'] {
-				transform: scale(1.2);
-			}
-
-			&:hover {
-				cursor: pointer;
-				background: var(--bgColor);
-			}
-
-			h4 {
-				font-weight: 500;
-			}
-		}
-
-		> .favoriteListHeader {
-			padding: 0 20px;
-			display: flex;
-			align-items: center;
-			gap: 20px;
-			width: 100%;
-			height: 35px;
-			background: var(--bgColor2);
-
-			input[type='checkbox'] {
-				transform: scale(1.2);
-			}
-
-			h4 {
-				width: 100%;
-				display: flex;
-				justify-content: center;
-				font-weight: 500;
-			}
-		}
-
-		&::-webkit-scrollbar {
-			width: 3px;
-		}
-
-		&::-webkit-scrollbar-thumb {
-			width: 3px;
-			background: #ccc;
-			visibility: hidden;
-		}
-
-		&:hover::-webkit-scrollbar-thumb {
-			visibility: visible;
-		}
-	}
-
-	.item:last-child {
-		border-bottom: 1px solid #ccc;
-	}
-`;
-
-// 위아래패딩은 30px로 가봅시다.
-const RightContainer = styled.div`
-	width: 40%;
-	height: 100%;
-	overflow: hidden;
-	background-color: var(--bgColor);
-	padding: 20px 40px;
-
-	// 100 85 60
-	> .header {
-		height: 100px;
-
-		h2 {
-			font-weight: 500;
-			font-size: 2rem;
-		}
-		span {
-			opacity: 0.8;
-		}
-	}
-
-	> .contextName {
-		height: 85px;
-		display: flex;
-		flex-direction: column;
-		justify-content: space-between;
-
-		> div {
-			display: flex;
-			justify-content: space-between;
-			margin-bottom: 1px;
-			position: relative;
-			height: 40px;
-
-			h3 {
-				display: flex;
-				align-items: center;
-				font-weight: 500;
-			}
-		}
-
-		input {
-			width: 100%;
-			padding: 10px 10px;
-			border-radius: 8px;
-			border: 1px solid var(--bgColor2);
-
-			&::placeholder {
-				opacity: 0.8;
-			}
-
-			&:focus {
-				outline: none;
-			}
-		}
-	}
-
-	> .createButtonWrapper {
-		text-align: right;
-
-		button {
-			height: 40px;
-			padding: 0 10px;
-			border: none;
-			background: #222;
-			color: white;
-
-			&:hover {
-				cursor: pointer;
-			}
-		}
-	}
-`;
+import DashHeader from '../dashheader/DashHeader';
+import CreateContextSection from '../createContextSection/CreateContextSection';
 
 export default function IndicatorsTab() {
-	const { deleteFavoriteMutationAll } = useFavoriteMutation();
 	const { allFavorites_List } = useFavoriteQuery();
 	const [currentCategoryId, setCurrentCategoryId] = useState<number>(const_categoryId.interest_mortgage);
 	const [checkedFavorite_List, setCheckedFavorite_List] = useState<FavoriteIndicator_Type[]>([]);
@@ -214,6 +43,7 @@ export default function IndicatorsTab() {
 
 	return (
 		<FavoriteContainer>
+			<DashHeader />
 			<LeftContainer>
 				<div className='categoryTabMenuWrapper'>
 					<CategoryTabMenu categoryIdList={categoryIds} selectedCategoryId={currentCategoryId} setSelectedCategoryId={setCurrentCategoryId} />
